854b99b6c7b9fe5c1c1cea0d526148cc272a2f13
Paddy <paddy@hashicorp.com>
2021-06-01 10:20:17 -0700

    Add primitive types. (#29)
    This adds to #28 by creating implementations of the attr.Type interface
    for our primitive types: bools, numbers, and strings.

---
+++ types/bool.go
@@ -0,0 +1,80 @@
+package types
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+)
+
+func boolValueFromTerraform(ctx context.Context, in tftypes.Value) (attr.Value, error) {
+	val := new(Bool)
+	err := val.SetTerraformValue(ctx, in)
+	return val, err
+}
+
+var _ attr.Value = &Bool{}
+
+// Bool represents a boolean value.
+type Bool struct {
+	// Unknown will be true if the value is not yet known.
+	Unknown bool
+
+	// Null will be true if the value was not set, or was explicitly set to
+	// null.
+	Null bool
+
+	// Value contains the set value, as long as Unknown and Null are both
+	// false.
+	Value bool
+}
+
+// ToTerraformValue returns the data contained in the *Bool as a bool. If
+// Unknown is true, it returns a tftypes.UnknownValue. If Null is true, it
+// returns nil.
+func (b *Bool) ToTerraformValue(_ context.Context) (interface{}, error) {
+	if b.Null {
+		return nil, nil
+	}
+	if b.Unknown {
+		return tftypes.UnknownValue, nil
+	}
+	return b.Value, nil
+}
+
+// Equal returns true if `other` is a *Bool and has the same value as `b`.
+func (b *Bool) Equal(other attr.Value) bool {
+	if b == nil && other == nil {
+		return true
+	}
+	if b == nil || other == nil {
+		return false
+	}
+	o, ok := other.(*Bool)
+	if !ok {
+		return false
+	}
+	if b.Unknown != o.Unknown {
+		return false
+	}
+	if b.Null != o.Null {
+		return false
+	}
+	return b.Value == o.Value
+}
+
+// SetTerraformValue updates the Bool to match the contents of `val`.
+func (b *Bool) SetTerraformValue(ctx context.Context, val tftypes.Value) error {
+	b.Unknown = false
+	b.Null = false
+	b.Value = false
+	if val.IsNull() {
+		b.Null = true
+		return nil
+	}
+	if !val.IsKnown() {
+		b.Unknown = true
+		return nil
+	}
+	return val.As(&b.Value)
+}

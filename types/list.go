37dbc4620ac987e054d4d85be842b216d48a8bf6
Paddy Carver <paddy@hashicorp.com>
2021-06-01 19:46:49 -0700

    Add support for list types.
    Added a list type, with tests. The tests are not exhaustive, but cover
    all the code paths for now.

---
+++ types/list.go
@@ -0,0 +1,197 @@
+package types
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/internal/reflect"
+
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+)
+
+var (
+	_ attr.Type  = ListType{}
+	_ attr.Value = &List{}
+)
+
+// ListType is an AttributeType representing a list of values. All values must
+// be of the same type, which the provider must specify as the ElemType
+// property.
+type ListType struct {
+	ElemType attr.Type
+}
+
+// TerraformType returns the tftypes.Type that should be used to
+// represent this type. This constrains what user input will be
+// accepted and what kind of data can be set in state. The framework
+// will use this to translate the AttributeType to something Terraform
+// can understand.
+func (l ListType) TerraformType(ctx context.Context) tftypes.Type {
+	return tftypes.List{
+		ElementType: l.ElemType.TerraformType(ctx),
+	}
+}
+
+// ValueFromTerraform returns an AttributeValue given a tftypes.Value.
+// This is meant to convert the tftypes.Value into a more convenient Go
+// type for the provider to consume the data with.
+func (l ListType) ValueFromTerraform(ctx context.Context, in tftypes.Value) (attr.Value, error) {
+	list := &List{
+		ElemType: l.ElemType,
+	}
+	err := list.SetTerraformValue(ctx, in)
+	return list, err
+}
+
+// Equal returns true if `o` is also a ListType and has the same ElemType.
+func (l ListType) Equal(o attr.Type) bool {
+	if l.ElemType == nil {
+		return false
+	}
+	other, ok := o.(ListType)
+	if !ok {
+		return false
+	}
+	return l.ElemType.Equal(other.ElemType)
+}
+
+// List represents a list of AttributeValues, all of the same type, indicated
+// by ElemType.
+type List struct {
+	// Unknown will be set to true if the entire list is an unknown value.
+	// If only some of the elements in the list are unknown, their known or
+	// unknown status will be represented however that AttributeValue
+	// surfaces that information. The List's Unknown property only tracks
+	// if the number of elements in a List is known, not whether the
+	// elements that are in the list are known.
+	Unknown bool
+
+	// Null will be set to true if the list is null, either because it was
+	// omitted from the configuration, state, or plan, or because it was
+	// explicitly set to null.
+	Null bool
+
+	// Elems are the elements in the list.
+	Elems []attr.Value
+
+	// ElemType is the tftypes.Type of the elements in the list. All
+	// elements in the list must be of this type.
+	ElemType attr.Type
+}
+
+// ElementsAs populates `target` with the elements of the List, throwing an
+// error if the elements cannot be stored in `target`.
+func (l *List) ElementsAs(ctx context.Context, target interface{}, allowUnhandled bool) error {
+	// we need a tftypes.Value for this List to be able to use it with our
+	// reflection code
+	values := make([]tftypes.Value, 0, len(l.Elems))
+	for pos, elem := range l.Elems {
+		val, err := elem.ToTerraformValue(ctx)
+		if err != nil {
+			return fmt.Errorf("error getting Terraform value for element %d: %w", pos, err)
+		}
+		err = tftypes.ValidateValue(l.ElemType.TerraformType(ctx), val)
+		if err != nil {
+			return fmt.Errorf("error using created Terraform value for element %d: %w", pos, err)
+		}
+		values = append(values, tftypes.NewValue(l.ElemType.TerraformType(ctx), val))
+	}
+	return reflect.Into(ctx, tftypes.NewValue(tftypes.List{
+		ElementType: l.ElemType.TerraformType(ctx),
+	}, values), target, reflect.Options{
+		UnhandledNullAsEmpty:    allowUnhandled,
+		UnhandledUnknownAsEmpty: allowUnhandled,
+	})
+}
+
+// ToTerraformValue returns the data contained in the AttributeValue as
+// a Go type that tftypes.NewValue will accept.
+func (l *List) ToTerraformValue(ctx context.Context) (interface{}, error) {
+	if l.Unknown {
+		return tftypes.UnknownValue, nil
+	}
+	if l.Null {
+		return nil, nil
+	}
+	vals := make([]tftypes.Value, 0, len(l.Elems))
+	for _, elem := range l.Elems {
+		val, err := elem.ToTerraformValue(ctx)
+		if err != nil {
+			return nil, err
+		}
+		err = tftypes.ValidateValue(l.ElemType.TerraformType(ctx), val)
+		if err != nil {
+			return nil, err
+		}
+		vals = append(vals, tftypes.NewValue(l.ElemType.TerraformType(ctx), val))
+	}
+	return vals, nil
+}
+
+// Equal must return true if the AttributeValue is considered
+// semantically equal to the AttributeValue passed as an argument.
+func (l *List) Equal(o attr.Value) bool {
+	if l == nil {
+		return false
+	}
+	other, ok := o.(*List)
+	if !ok {
+		return false
+	}
+	if l.Unknown != other.Unknown {
+		return false
+	}
+	if l.Null != other.Null {
+		return false
+	}
+	if !l.ElemType.Equal(other.ElemType) {
+		return false
+	}
+	if len(l.Elems) != len(other.Elems) {
+		return false
+	}
+	for pos, lElem := range l.Elems {
+		oElem := other.Elems[pos]
+		if !lElem.Equal(oElem) {
+			return false
+		}
+	}
+	return true
+}
+
+// SetTerraformValue updates `l` to reflect the data stored in `in`.
+func (l *List) SetTerraformValue(ctx context.Context, in tftypes.Value) error {
+	l.Unknown = false
+	l.Null = false
+	l.Elems = nil
+	if !in.Type().Is(tftypes.List{}) {
+		return fmt.Errorf("can't use %s as value of List, can only use tftypes.List values", in.String())
+	}
+	if !in.Type().Is(tftypes.List{ElementType: l.ElemType.TerraformType(ctx)}) {
+		return fmt.Errorf("can't use %s as value of List with ElementType %T, can only use %s values", in.String(), l.ElemType, l.ElemType.TerraformType(ctx).String())
+	}
+	if !in.IsKnown() {
+		l.Unknown = true
+		return nil
+	}
+	if in.IsNull() {
+		l.Null = true
+		return nil
+	}
+	val := []tftypes.Value{}
+	err := in.As(&val)
+	if err != nil {
+		return err
+	}
+	elems := make([]attr.Value, 0, len(val))
+	for _, elem := range val {
+		av, err := l.ElemType.ValueFromTerraform(ctx, elem)
+		if err != nil {
+			return err
+		}
+		elems = append(elems, av)
+	}
+	l.Elems = elems
+	return nil
+}
---
+++ types/list_test.go
@@ -0,0 +1,677 @@
+package types
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+)
+
+func TestListTypeTerraformType(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		input    ListType
+		expected tftypes.Type
+	}
+	tests := map[string]testCase{
+		"list-of-strings": {
+			input: ListType{
+				ElemType: StringType,
+			},
+			expected: tftypes.List{
+				ElementType: tftypes.String,
+			},
+		},
+		"list-of-list-of-strings": {
+			input: ListType{
+				ElemType: ListType{
+					ElemType: StringType,
+				},
+			},
+			expected: tftypes.List{
+				ElementType: tftypes.List{
+					ElementType: tftypes.String,
+				},
+			},
+		},
+		"list-of-list-of-list-of-strings": {
+			input: ListType{
+				ElemType: ListType{
+					ElemType: ListType{
+						ElemType: StringType,
+					},
+				},
+			},
+			expected: tftypes.List{
+				ElementType: tftypes.List{
+					ElementType: tftypes.List{
+						ElementType: tftypes.String,
+					},
+				},
+			},
+		},
+	}
+	for name, test := range tests {
+		name, test := name, test
+		t.Run(name, func(t *testing.T) {
+			got := test.input.TerraformType(context.Background())
+			if !got.Is(test.expected) {
+				t.Errorf("Expected %s, got %s", test.expected, got)
+			}
+		})
+	}
+}
+
+func TestListTypeValueFromTerraform(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		receiver    ListType
+		input       tftypes.Value
+		expected    attr.Value
+		expectedErr string
+	}
+	tests := map[string]testCase{
+		"list-of-strings": {
+			receiver: ListType{
+				ElemType: StringType,
+			},
+			input: tftypes.NewValue(tftypes.List{
+				ElementType: tftypes.String,
+			}, []tftypes.Value{
+				tftypes.NewValue(tftypes.String, "hello"),
+				tftypes.NewValue(tftypes.String, "world"),
+			}),
+			expected: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Value: "world"},
+				},
+			},
+		},
+		"unknown-list": {
+			receiver: ListType{
+				ElemType: StringType,
+			},
+			input: tftypes.NewValue(tftypes.List{
+				ElementType: tftypes.String,
+			}, tftypes.UnknownValue),
+			expected: &List{
+				ElemType: StringType,
+				Unknown:  true,
+			},
+		},
+		"partially-unknown-list": {
+			receiver: ListType{
+				ElemType: StringType,
+			},
+			input: tftypes.NewValue(tftypes.List{
+				ElementType: tftypes.String,
+			}, []tftypes.Value{
+				tftypes.NewValue(tftypes.String, "hello"),
+				tftypes.NewValue(tftypes.String, tftypes.UnknownValue),
+			}),
+			expected: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Unknown: true},
+				},
+			},
+		},
+		"null-list": {
+			receiver: ListType{
+				ElemType: StringType,
+			},
+			input: tftypes.NewValue(tftypes.List{
+				ElementType: tftypes.String,
+			}, nil),
+			expected: &List{
+				ElemType: StringType,
+				Null:     true,
+			},
+		},
+		"partially-null-list": {
+			receiver: ListType{
+				ElemType: StringType,
+			},
+			input: tftypes.NewValue(tftypes.List{
+				ElementType: tftypes.String,
+			}, []tftypes.Value{
+				tftypes.NewValue(tftypes.String, "hello"),
+				tftypes.NewValue(tftypes.String, nil),
+			}),
+			expected: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Null: true},
+				},
+			},
+		},
+	}
+	for name, test := range tests {
+		name, test := name, test
+		t.Run(name, func(t *testing.T) {
+			got, gotErr := test.receiver.ValueFromTerraform(context.Background(), test.input)
+			if gotErr != nil {
+				if test.expectedErr != "" {
+					if gotErr.Error() != test.expectedErr {
+						t.Errorf("Expected error to be %q, got %q", test.expectedErr, gotErr.Error())
+						return
+					}
+				}
+				t.Errorf("Unexpected error: %s", gotErr.Error())
+				return
+			}
+			if gotErr == nil && test.expectedErr != "" {
+				t.Errorf("Expected error to be %q, got nil", test.expectedErr)
+				return
+			}
+			if diff := cmp.Diff(got, test.expected); diff != "" {
+				t.Errorf("Unexpected diff (-expected, +got): %s", diff)
+			}
+		})
+	}
+}
+
+func TestListTypeEqual(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		receiver ListType
+		input    attr.Type
+		expected bool
+	}
+	tests := map[string]testCase{
+		"equal": {
+			receiver: ListType{ElemType: StringType},
+			input:    ListType{ElemType: StringType},
+			expected: true,
+		},
+		"diff": {
+			receiver: ListType{ElemType: StringType},
+			input:    ListType{ElemType: NumberType},
+			expected: false,
+		},
+		"wrongType": {
+			receiver: ListType{ElemType: StringType},
+			input:    NumberType,
+			expected: false,
+		},
+		"nil": {
+			receiver: ListType{ElemType: StringType},
+			input:    nil,
+			expected: false,
+		},
+		"nil-elem": {
+			receiver: ListType{},
+			input:    ListType{},
+			// ListTypes with nil ElemTypes are invalid, and
+			// aren't equal to anything
+			expected: false,
+		},
+	}
+	for name, test := range tests {
+		name, test := name, test
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := test.receiver.Equal(test.input)
+			if test.expected != got {
+				t.Errorf("Expected %v, got %v", test.expected, got)
+			}
+		})
+	}
+}
+
+func TestListElementsAs_stringSlice(t *testing.T) {
+	t.Parallel()
+
+	var stringSlice []string
+	expected := []string{"hello", "world"}
+
+	err := (&List{
+		ElemType: StringType,
+		Elems: []attr.Value{
+			&String{Value: "hello"},
+			&String{Value: "world"},
+		}}).ElementsAs(context.Background(), &stringSlice, false)
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err.Error())
+	}
+	if diff := cmp.Diff(stringSlice, expected); diff != "" {
+		t.Errorf("Unexpected diff (-expected, +got): %s", diff)
+	}
+}
+
+func TestListElementsAs_attributeValueSlice(t *testing.T) {
+	t.Parallel()
+
+	var stringSlice []*String
+	expected := []*String{
+		{Value: "hello"},
+		{Value: "world"},
+	}
+
+	err := (&List{
+		ElemType: StringType,
+		Elems: []attr.Value{
+			&String{Value: "hello"},
+			&String{Value: "world"},
+		}}).ElementsAs(context.Background(), &stringSlice, false)
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err.Error())
+	}
+	if diff := cmp.Diff(stringSlice, expected); diff != "" {
+		t.Errorf("Unexpected diff (-expected, +got): %s", diff)
+	}
+}
+
+func TestListToTerraformValue(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		input       *List
+		expectation interface{}
+	}
+	tests := map[string]testCase{
+		"value": {
+			input: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Value: "world"},
+				},
+			},
+			expectation: []tftypes.Value{
+				tftypes.NewValue(tftypes.String, "hello"),
+				tftypes.NewValue(tftypes.String, "world"),
+			},
+		},
+		"unknown": {
+			input:       &List{Unknown: true},
+			expectation: tftypes.UnknownValue,
+		},
+		"null": {
+			input:       &List{Null: true},
+			expectation: nil,
+		},
+		"partial-unknown": {
+			input: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Unknown: true},
+					&String{Value: "hello, world"},
+				},
+			},
+			expectation: []tftypes.Value{
+				tftypes.NewValue(tftypes.String, tftypes.UnknownValue),
+				tftypes.NewValue(tftypes.String, "hello, world"),
+			},
+		},
+		"partial-null": {
+			input: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Null: true},
+					&String{Value: "hello, world"},
+				},
+			},
+			expectation: []tftypes.Value{
+				tftypes.NewValue(tftypes.String, nil),
+				tftypes.NewValue(tftypes.String, "hello, world"),
+			},
+		},
+	}
+	for name, test := range tests {
+		name, test := name, test
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got, err := test.input.ToTerraformValue(context.Background())
+			if err != nil {
+				t.Errorf("Unexpected error: %s", err)
+				return
+			}
+			if diff := cmp.Diff(got, test.expectation); diff != "" {
+				t.Errorf("Unexpected result (+got, -expected): %s", diff)
+			}
+		})
+	}
+}
+
+func TestListEqual(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		receiver *List
+		input    attr.Value
+		expected bool
+	}
+	tests := map[string]testCase{
+		"list-value-list-value": {
+			receiver: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Value: "world"},
+				},
+			},
+			input: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Value: "world"},
+				},
+			},
+			expected: true,
+		},
+		"list-value-diff": {
+			receiver: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Value: "world"},
+				},
+			},
+			input: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "goodnight"},
+					&String{Value: "moon"},
+				},
+			},
+			expected: false,
+		},
+		"list-value-count-diff": {
+			receiver: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Value: "world"},
+				},
+			},
+			input: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Value: "world"},
+					&String{Value: "test"},
+				},
+			},
+			expected: false,
+		},
+		"list-value-type-diff": {
+			receiver: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Value: "world"},
+				},
+			},
+			input: &List{
+				ElemType: BoolType,
+				Elems: []attr.Value{
+					&Bool{Value: false},
+					&Bool{Value: true},
+				},
+			},
+			expected: false,
+		},
+		"list-value-unknown": {
+			receiver: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Value: "world"},
+				},
+			},
+			input:    &List{Unknown: true},
+			expected: false,
+		},
+		"list-value-null": {
+			receiver: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Value: "world"},
+				},
+			},
+			input:    &List{Null: true},
+			expected: false,
+		},
+		"list-value-wrongType": {
+			receiver: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Value: "world"},
+				},
+			},
+			input:    &String{Value: "hello, world"},
+			expected: false,
+		},
+		"list-value-nil": {
+			receiver: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Value: "world"},
+				},
+			},
+			input:    nil,
+			expected: false,
+		},
+		"partially-known-list-value-list-value": {
+			receiver: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Unknown: true},
+				},
+			},
+			input: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Unknown: true},
+				},
+			},
+			expected: true,
+		},
+		"partially-known-list-value-diff": {
+			receiver: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Unknown: true},
+				},
+			},
+			input: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Value: "world"},
+				},
+			},
+			expected: false,
+		},
+		"partially-known-list-value-unknown": {
+			receiver: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Unknown: true},
+				},
+			},
+			input:    &List{Unknown: true},
+			expected: false,
+		},
+		"partially-known-list-value-null": {
+			receiver: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Unknown: true},
+				},
+			},
+			input:    &List{Null: true},
+			expected: false,
+		},
+		"partially-known-list-value-wrongType": {
+			receiver: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Unknown: true},
+				},
+			},
+			input:    &String{Value: "hello, world"},
+			expected: false,
+		},
+		"partially-known-list-value-nil": {
+			receiver: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Unknown: true},
+				},
+			},
+			input:    nil,
+			expected: false,
+		},
+		"partially-null-list-value-list-value": {
+			receiver: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Null: true},
+				},
+			},
+			input: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Null: true},
+				},
+			},
+			expected: true,
+		},
+		"partially-null-list-value-diff": {
+			receiver: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Null: true},
+				},
+			},
+			input: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Value: "world"},
+				},
+			},
+			expected: false,
+		},
+		"partially-null-list-value-unknown": {
+			receiver: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Null: true},
+				},
+			},
+			input: &List{
+				Unknown: true,
+			},
+			expected: false,
+		},
+		"partially-null-list-value-null": {
+			receiver: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Null: true},
+				},
+			},
+			input: &List{
+				Null: true,
+			},
+			expected: false,
+		},
+		"partially-null-list-value-wrongType": {
+			receiver: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Null: true},
+				},
+			},
+			input:    &String{Value: "hello, world"},
+			expected: false,
+		},
+		"partially-null-list-value-nil": {
+			receiver: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Null: true},
+				},
+			},
+			input:    nil,
+			expected: false,
+		},
+		"nil-list-value": {
+			receiver: nil,
+			input: &List{
+				ElemType: StringType,
+				Elems: []attr.Value{
+					&String{Value: "hello"},
+					&String{Null: true},
+				},
+			},
+			expected: false,
+		},
+		"nil-unknown": {
+			receiver: nil,
+			input:    &List{Unknown: true},
+			expected: false,
+		},
+		"nil-null": {
+			receiver: nil,
+			input:    &List{Null: true},
+			expected: false,
+		},
+		"nil-wrongType": {
+			receiver: nil,
+			input:    &String{Value: "hello, world"},
+			expected: false,
+		},
+		"nil-nil": {
+			receiver: nil,
+			input:    nil,
+			expected: false,
+		},
+	}
+	for name, test := range tests {
+		name, test := name, test
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := test.receiver.Equal(test.input)
+			if got != test.expected {
+				t.Errorf("Expected %v, got %v", test.expected, got)
+			}
+		})
+	}
+}
+
+func TestListSetTerraformValue(t *testing.T) {
+	t.Parallel()
+}

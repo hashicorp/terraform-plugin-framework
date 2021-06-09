f294107ec36a33d2a00bec41387b72ee4986de13
Katy Moe <katy@katy.moe>
2021-06-09 16:15:19 +0100

    add Config and Plan types

---
+++ config.go
@@ -0,0 +1,51 @@
+package tfsdk
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/internal/reflect"
+	"github.com/hashicorp/terraform-plugin-framework/schema"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+)
+
+// Config represents a Terraform config.
+type Config struct {
+	Raw    tftypes.Value
+	Schema schema.Schema
+}
+
+// Get populates the struct passed as `target` with the entire config.
+func (c Config) Get(ctx context.Context, target interface{}) error {
+	return reflect.Into(ctx, c.Schema.AttributeType(), c.Raw, target, reflect.Options{})
+}
+
+// GetAttribute retrieves the attribute found at `path` and returns it as an
+// attr.Value. Consumers should assert the type of the returned value with the
+// desired attr.Type.
+func (c Config) GetAttribute(ctx context.Context, path *tftypes.AttributePath) (attr.Value, error) {
+	attrType, err := c.Schema.AttributeTypeAtPath(path)
+	if err != nil {
+		return nil, fmt.Errorf("error walking schema: %w", err)
+	}
+
+	attrValue, err := c.terraformValueAtPath(path)
+	if err != nil {
+		return nil, fmt.Errorf("error walking config: %w", err)
+	}
+
+	return attrType.ValueFromTerraform(ctx, *attrValue)
+}
+
+func (c Config) terraformValueAtPath(path *tftypes.AttributePath) (*tftypes.Value, error) {
+	rawValue, remaining, err := tftypes.WalkAttributePath(c.Raw, path)
+	if err != nil {
+		return nil, fmt.Errorf("%v still remains in the path: %w", remaining, err)
+	}
+	attrValue, ok := rawValue.(tftypes.Value)
+	if !ok {
+		return nil, fmt.Errorf("got non-tftypes.Value result %v", rawValue)
+	}
+	return &attrValue, err
+}
---
+++ plan.go
@@ -0,0 +1,51 @@
+package tfsdk
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/internal/reflect"
+	"github.com/hashicorp/terraform-plugin-framework/schema"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+)
+
+// Plan represents a Terraform plan.
+type Plan struct {
+	Raw    tftypes.Value
+	Schema schema.Schema
+}
+
+// Get populates the struct passed as `target` with the entire plan.
+func (p Plan) Get(ctx context.Context, target interface{}) error {
+	return reflect.Into(ctx, p.Schema.AttributeType(), p.Raw, target, reflect.Options{})
+}
+
+// GetAttribute retrieves the attribute found at `path` and returns it as an
+// attr.Value. Consumers should assert the type of the returned value with the
+// desired attr.Type.
+func (p Plan) GetAttribute(ctx context.Context, path *tftypes.AttributePath) (attr.Value, error) {
+	attrType, err := p.Schema.AttributeTypeAtPath(path)
+	if err != nil {
+		return nil, fmt.Errorf("error walking schema: %w", err)
+	}
+
+	attrValue, err := p.terraformValueAtPath(path)
+	if err != nil {
+		return nil, fmt.Errorf("error walking plan: %w", err)
+	}
+
+	return attrType.ValueFromTerraform(ctx, *attrValue)
+}
+
+func (p Plan) terraformValueAtPath(path *tftypes.AttributePath) (*tftypes.Value, error) {
+	rawValue, remaining, err := tftypes.WalkAttributePath(p.Raw, path)
+	if err != nil {
+		return nil, fmt.Errorf("%v still remains in the path: %w", remaining, err)
+	}
+	attrValue, ok := rawValue.(tftypes.Value)
+	if !ok {
+		return nil, fmt.Errorf("got non-tftypes.Value result %v", rawValue)
+	}
+	return &attrValue, err
+}

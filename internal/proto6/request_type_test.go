2a3165067c60d02fac9e9fd92407c4759bef009d
Paddy Carver <paddy@hashicorp.com>
2021-06-21 13:30:12 -0700

    Refactor request type tests.
    A little less repetition.

--- internal/proto6/request_type_test.go
+++ internal/proto6/request_type_test.go
@@ -8,13 +8,15 @@ import (
 	"github.com/hashicorp/terraform-plugin-go/tftypes"
 )
 
-func TestIsCreate(t *testing.T) {
+func TestRequestType(t *testing.T) {
 	t.Parallel()
 
 	type testCase struct {
 		req         *tfprotov6.ApplyResourceChangeRequest
 		typ         tftypes.Type
-		expected    bool
+		isCreate    bool
+		isUpdate    bool
+		isDestroy   bool
 		expectedErr string
 	}
 
@@ -49,8 +51,10 @@ func TestIsCreate(t *testing.T) {
 				PriorState:   &nullDV,
 				PlannedState: &setDV,
 			},
-			typ:      reqType,
-			expected: true,
+			typ:       reqType,
+			isCreate:  true,
+			isUpdate:  false,
+			isDestroy: false,
 		},
 		"set-prior-state-set-planned-state": {
 			req: &tfprotov6.ApplyResourceChangeRequest{
@@ -58,8 +62,10 @@ func TestIsCreate(t *testing.T) {
 				PriorState:   &setDV,
 				PlannedState: &setDV,
 			},
-			typ:      reqType,
-			expected: false,
+			typ:       reqType,
+			isCreate:  false,
+			isUpdate:  true,
+			isDestroy: false,
 		},
 		"set-prior-state-null-planned-state": {
 			req: &tfprotov6.ApplyResourceChangeRequest{
@@ -67,8 +73,10 @@ func TestIsCreate(t *testing.T) {
 				PriorState:   &setDV,
 				PlannedState: &nullDV,
 			},
-			typ:      reqType,
-			expected: false,
+			typ:       reqType,
+			isCreate:  false,
+			isUpdate:  false,
+			isDestroy: true,
 		},
 	}
 
@@ -94,84 +102,12 @@ func TestIsCreate(t *testing.T) {
 				t.Errorf("Expected error to be %q, got nil", tc.expectedErr)
 				return
 			}
-			if tc.expected != got {
-				t.Errorf("Expected %v, got %v", tc.expected, got)
+			if tc.isCreate != got {
+				t.Errorf("Expected IsCreate to return %v, got %v", tc.isCreate, got)
 				return
 			}
-		})
-	}
-}
 
-func TestIsUpdate(t *testing.T) {
-	t.Parallel()
-
-	type testCase struct {
-		req         *tfprotov6.ApplyResourceChangeRequest
-		typ         tftypes.Type
-		expected    bool
-		expectedErr string
-	}
-
-	reqType := tftypes.Object{
-		AttributeTypes: map[string]tftypes.Type{
-			"a": tftypes.String,
-			"b": tftypes.Number,
-			"c": tftypes.Bool,
-		},
-	}
-
-	setDV, err := tfprotov6.NewDynamicValue(reqType, tftypes.NewValue(reqType, map[string]tftypes.Value{
-		"a": tftypes.NewValue(tftypes.String, "hello, world"),
-		"b": tftypes.NewValue(tftypes.Number, 123),
-		"c": tftypes.NewValue(tftypes.Bool, true),
-	}))
-	if err != nil {
-		t.Errorf("Unexpected error creating set dynamic value: %s", err)
-		return
-	}
-
-	nullDV, err := tfprotov6.NewDynamicValue(reqType, tftypes.NewValue(reqType, nil))
-	if err != nil {
-		t.Errorf("Unexpected error creating null dynamic value: %s", err)
-		return
-	}
-
-	tests := map[string]testCase{
-		"null-prior-state-set-planned-state": {
-			req: &tfprotov6.ApplyResourceChangeRequest{
-				TypeName:     "foo",
-				PriorState:   &nullDV,
-				PlannedState: &setDV,
-			},
-			typ:      reqType,
-			expected: false,
-		},
-		"set-prior-state-set-planned-state": {
-			req: &tfprotov6.ApplyResourceChangeRequest{
-				TypeName:     "foo",
-				PriorState:   &setDV,
-				PlannedState: &setDV,
-			},
-			typ:      reqType,
-			expected: true,
-		},
-		"set-prior-state-null-planned-state": {
-			req: &tfprotov6.ApplyResourceChangeRequest{
-				TypeName:     "foo",
-				PriorState:   &setDV,
-				PlannedState: &nullDV,
-			},
-			typ:      reqType,
-			expected: false,
-		},
-	}
-
-	for name, tc := range tests {
-		name, tc := name, tc
-		t.Run(name, func(t *testing.T) {
-			t.Parallel()
-
-			got, err := IsUpdate(context.Background(), tc.req, tc.typ)
+			got, err = IsUpdate(context.Background(), tc.req, tc.typ)
 			if err != nil {
 				if tc.expectedErr == "" {
 					t.Errorf("Unexpected error: %s", err)
@@ -188,84 +124,12 @@ func TestIsUpdate(t *testing.T) {
 				t.Errorf("Expected error to be %q, got nil", tc.expectedErr)
 				return
 			}
-			if tc.expected != got {
-				t.Errorf("Expected %v, got %v", tc.expected, got)
+			if tc.isUpdate != got {
+				t.Errorf("Expected IsUpdate to return %v, got %v", tc.isUpdate, got)
 				return
 			}
-		})
-	}
-}
 
-func TestIsDestroy(t *testing.T) {
-	t.Parallel()
-
-	type testCase struct {
-		req         *tfprotov6.ApplyResourceChangeRequest
-		typ         tftypes.Type
-		expected    bool
-		expectedErr string
-	}
-
-	reqType := tftypes.Object{
-		AttributeTypes: map[string]tftypes.Type{
-			"a": tftypes.String,
-			"b": tftypes.Number,
-			"c": tftypes.Bool,
-		},
-	}
-
-	setDV, err := tfprotov6.NewDynamicValue(reqType, tftypes.NewValue(reqType, map[string]tftypes.Value{
-		"a": tftypes.NewValue(tftypes.String, "hello, world"),
-		"b": tftypes.NewValue(tftypes.Number, 123),
-		"c": tftypes.NewValue(tftypes.Bool, true),
-	}))
-	if err != nil {
-		t.Errorf("Unexpected error creating set dynamic value: %s", err)
-		return
-	}
-
-	nullDV, err := tfprotov6.NewDynamicValue(reqType, tftypes.NewValue(reqType, nil))
-	if err != nil {
-		t.Errorf("Unexpected error creating null dynamic value: %s", err)
-		return
-	}
-
-	tests := map[string]testCase{
-		"null-prior-state-set-planned-state": {
-			req: &tfprotov6.ApplyResourceChangeRequest{
-				TypeName:     "foo",
-				PriorState:   &nullDV,
-				PlannedState: &setDV,
-			},
-			typ:      reqType,
-			expected: false,
-		},
-		"set-prior-state-set-planned-state": {
-			req: &tfprotov6.ApplyResourceChangeRequest{
-				TypeName:     "foo",
-				PriorState:   &setDV,
-				PlannedState: &setDV,
-			},
-			typ:      reqType,
-			expected: false,
-		},
-		"set-prior-state-null-planned-state": {
-			req: &tfprotov6.ApplyResourceChangeRequest{
-				TypeName:     "foo",
-				PriorState:   &setDV,
-				PlannedState: &nullDV,
-			},
-			typ:      reqType,
-			expected: true,
-		},
-	}
-
-	for name, tc := range tests {
-		name, tc := name, tc
-		t.Run(name, func(t *testing.T) {
-			t.Parallel()
-
-			got, err := IsDestroy(context.Background(), tc.req, tc.typ)
+			got, err = IsDestroy(context.Background(), tc.req, tc.typ)
 			if err != nil {
 				if tc.expectedErr == "" {
 					t.Errorf("Unexpected error: %s", err)
@@ -282,8 +146,8 @@ func TestIsDestroy(t *testing.T) {
 				t.Errorf("Expected error to be %q, got nil", tc.expectedErr)
 				return
 			}
-			if tc.expected != got {
-				t.Errorf("Expected %v, got %v", tc.expected, got)
+			if tc.isDestroy != got {
+				t.Errorf("Expected IsDestroy to return %v, got %v", tc.isDestroy, got)
 				return
 			}
 		})

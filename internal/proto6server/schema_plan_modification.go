d020d18a7fd51ba1373ce9162ba7a16567cbfbed
Brian Flad <bflad417@gmail.com>
2022-05-17 16:12:31 -0500

    internal: Remove nolint comments for Block usage

--- internal/proto6server/schema_plan_modification.go
+++ internal/proto6server/schema_plan_modification.go
@@ -61,7 +61,6 @@ func SchemaModifyPlan(ctx context.Context, s tfsdk.Schema, req ModifySchemaPlanR
 		AttributeModifyPlan(ctx, attr, attrReq, resp)
 	}
 
-	//nolint:staticcheck // Block support is required within the framework.
 	for name, block := range s.Blocks {
 		blockReq := tfsdk.ModifyAttributePlanRequest{
 			AttributePath: tftypes.NewAttributePath().WithAttributeName(name),

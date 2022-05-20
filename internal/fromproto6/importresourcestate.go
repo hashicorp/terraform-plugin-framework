04900f3b7b90c1bbb76c0e23c002e8b57eea88b6
Brian Flad <bflad417@gmail.com>
2022-05-20 09:17:18 -0500

    internal: Migrate ImportResourceState RPC to fromproto6, fwserver, and toproto6 (#337)
    Reference: https://github.com/hashicorp/terraform-plugin-framework/issues/215

---
+++ internal/fromproto6/importresourcestate.go
@@ -0,0 +1,47 @@
+package fromproto6
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/internal/fwserver"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+)
+
+// ImportResourceStateRequest returns the *fwserver.ImportResourceStateRequest
+// equivalent of a *tfprotov6.ImportResourceStateRequest.
+func ImportResourceStateRequest(ctx context.Context, proto6 *tfprotov6.ImportResourceStateRequest, resourceType tfsdk.ResourceType, resourceSchema *tfsdk.Schema) (*fwserver.ImportResourceStateRequest, diag.Diagnostics) {
+	if proto6 == nil {
+		return nil, nil
+	}
+
+	var diags diag.Diagnostics
+
+	// Panic prevention here to simplify the calling implementations.
+	// This should not happen, but just in case.
+	if resourceSchema == nil {
+		diags.AddError(
+			"Unable to Create Empty State",
+			"An unexpected error was encountered when creating the empty state. "+
+				"This is always an issue in the Terraform Provider SDK used to implement the provider and should be reported to the provider developers.\n\n"+
+				"Please report this to the provider developer:\n\n"+
+				"Missing schema.",
+		)
+
+		return nil, diags
+	}
+
+	fw := &fwserver.ImportResourceStateRequest{
+		EmptyState: tfsdk.State{
+			Raw:    tftypes.NewValue(resourceSchema.TerraformType(ctx), nil),
+			Schema: *resourceSchema,
+		},
+		ID:           proto6.ID,
+		ResourceType: resourceType,
+		TypeName:     proto6.TypeName,
+	}
+
+	return fw, diags
+}

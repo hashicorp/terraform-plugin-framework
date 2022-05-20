68c29df5f56cefa56ff0921260ba69c5509c905e
Brian Flad <bflad417@gmail.com>
2022-05-20 15:50:34 -0400

    internal: Migrate UpgradeResourceState RPC to fromproto6, fwserver, and toproto6
    Reference: https://github.com/hashicorp/terraform-plugin-framework/issues/215
    Reference: https://github.com/hashicorp/terraform-plugin-framework/issues/340

---
+++ internal/toproto6/upgraderesourcestate_test.go
@@ -0,0 +1,148 @@
+package toproto6_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/internal/fwserver"
+	"github.com/hashicorp/terraform-plugin-framework/internal/toproto6"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+)
+
+func TestUpgradeResourceStateResponse(t *testing.T) {
+	t.Parallel()
+
+	testProto6Type := tftypes.Object{
+		AttributeTypes: map[string]tftypes.Type{
+			"test_attribute": tftypes.String,
+		},
+	}
+
+	testProto6Value := tftypes.NewValue(testProto6Type, map[string]tftypes.Value{
+		"test_attribute": tftypes.NewValue(tftypes.String, "test-value"),
+	})
+
+	testProto6DynamicValue, err := tfprotov6.NewDynamicValue(testProto6Type, testProto6Value)
+
+	if err != nil {
+		t.Fatalf("unexpected error calling tfprotov6.NewDynamicValue(): %s", err)
+	}
+
+	testState := &tfsdk.State{
+		Raw: testProto6Value,
+		Schema: tfsdk.Schema{
+			Attributes: map[string]tfsdk.Attribute{
+				"test_attribute": {
+					Required: true,
+					Type:     types.StringType,
+				},
+			},
+		},
+	}
+
+	testStateInvalid := &tfsdk.State{
+		Raw: testProto6Value,
+		Schema: tfsdk.Schema{
+			Attributes: map[string]tfsdk.Attribute{
+				"test_attribute": {
+					Required: true,
+					Type:     types.BoolType,
+				},
+			},
+		},
+	}
+
+	testCases := map[string]struct {
+		input    *fwserver.UpgradeResourceStateResponse
+		expected *tfprotov6.UpgradeResourceStateResponse
+	}{
+		"nil": {
+			input:    nil,
+			expected: nil,
+		},
+		"empty": {
+			input:    &fwserver.UpgradeResourceStateResponse{},
+			expected: &tfprotov6.UpgradeResourceStateResponse{},
+		},
+		"diagnostics": {
+			input: &fwserver.UpgradeResourceStateResponse{
+				Diagnostics: diag.Diagnostics{
+					diag.NewWarningDiagnostic("test warning summary", "test warning details"),
+					diag.NewErrorDiagnostic("test error summary", "test error details"),
+				},
+			},
+			expected: &tfprotov6.UpgradeResourceStateResponse{
+				Diagnostics: []*tfprotov6.Diagnostic{
+					{
+						Severity: tfprotov6.DiagnosticSeverityWarning,
+						Summary:  "test warning summary",
+						Detail:   "test warning details",
+					},
+					{
+						Severity: tfprotov6.DiagnosticSeverityError,
+						Summary:  "test error summary",
+						Detail:   "test error details",
+					},
+				},
+			},
+		},
+		"diagnostics-invalid-upgradedstate": {
+			input: &fwserver.UpgradeResourceStateResponse{
+				Diagnostics: diag.Diagnostics{
+					diag.NewWarningDiagnostic("test warning summary", "test warning details"),
+					diag.NewErrorDiagnostic("test error summary", "test error details"),
+				},
+				UpgradedState: testStateInvalid,
+			},
+			expected: &tfprotov6.UpgradeResourceStateResponse{
+				Diagnostics: []*tfprotov6.Diagnostic{
+					{
+						Severity: tfprotov6.DiagnosticSeverityWarning,
+						Summary:  "test warning summary",
+						Detail:   "test warning details",
+					},
+					{
+						Severity: tfprotov6.DiagnosticSeverityError,
+						Summary:  "test error summary",
+						Detail:   "test error details",
+					},
+					{
+						Severity: tfprotov6.DiagnosticSeverityError,
+						Summary:  "Unable to Convert State",
+						Detail: "An unexpected error was encountered when converting the state to the protocol type. " +
+							"This is always an issue in the Terraform Provider SDK used to implement the provider and should be reported to the provider developers.\n\n" +
+							"Please report this to the provider developer:\n\n" +
+							"AttributeName(\"test_attribute\"): unexpected value type string, tftypes.Bool values must be of type bool",
+					},
+				},
+			},
+		},
+		"upgradedstate": {
+			input: &fwserver.UpgradeResourceStateResponse{
+				UpgradedState: testState,
+			},
+			expected: &tfprotov6.UpgradeResourceStateResponse{
+				UpgradedState: &testProto6DynamicValue,
+			},
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := toproto6.UpgradeResourceStateResponse(context.Background(), testCase.input)
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}

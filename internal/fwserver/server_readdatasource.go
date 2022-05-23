b5c9f65c6b04974b0ad11fafa8a426960bfbbe2e
Brian Flad <bflad417@gmail.com>
2022-05-23 05:52:01 -0500

    internal: Migrate ReadDataSource RPC to fromproto6, fwserver, and toproto6 (#339)
    Reference: https://github.com/hashicorp/terraform-plugin-framework/issues/215

---
+++ internal/fwserver/server_readdatasource.go
@@ -0,0 +1,65 @@
+package fwserver
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/internal/logging"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+)
+
+// ReadDataSourceRequest is the framework server request for the
+// ReadDataSource RPC.
+type ReadDataSourceRequest struct {
+	Config         *tfsdk.Config
+	DataSourceType tfsdk.DataSourceType
+	ProviderMeta   *tfsdk.Config
+}
+
+// ReadDataSourceResponse is the framework server response for the
+// ReadDataSource RPC.
+type ReadDataSourceResponse struct {
+	Diagnostics diag.Diagnostics
+	State       *tfsdk.State
+}
+
+// ReadDataSource implements the framework server ReadDataSource RPC.
+func (s *Server) ReadDataSource(ctx context.Context, req *ReadDataSourceRequest, resp *ReadDataSourceResponse) {
+	if req == nil {
+		return
+	}
+
+	// Always instantiate new DataSource instances.
+	logging.FrameworkDebug(ctx, "Calling provider defined DataSourceType NewDataSource")
+	dataSource, diags := req.DataSourceType.NewDataSource(ctx, s.Provider)
+	logging.FrameworkDebug(ctx, "Called provider defined DataSourceType NewDataSource")
+
+	resp.Diagnostics.Append(diags...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	readReq := tfsdk.ReadDataSourceRequest{}
+	readResp := tfsdk.ReadDataSourceResponse{
+		State: tfsdk.State{
+			Schema: req.Config.Schema,
+		},
+	}
+
+	if req.Config != nil {
+		readReq.Config = *req.Config
+		readResp.State.Raw = req.Config.Raw.Copy()
+	}
+
+	if req.ProviderMeta != nil {
+		readReq.ProviderMeta = *req.ProviderMeta
+	}
+
+	logging.FrameworkDebug(ctx, "Calling provider defined DataSource Read")
+	dataSource.Read(ctx, readReq, &readResp)
+	logging.FrameworkDebug(ctx, "Called provider defined DataSource Read")
+
+	resp.Diagnostics = readResp.Diagnostics
+	resp.State = &readResp.State
+}
---
+++ internal/toproto6/readdatasource_test.go
@@ -0,0 +1,148 @@
+package toproto6_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/internal/fwserver"
+	"github.com/hashicorp/terraform-plugin-framework/internal/toproto6"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+)
+
+func TestReadDataSourceResponse(t *testing.T) {
+	t.Parallel()
+
+	testProto6Type := tftypes.Object{
+		AttributeTypes: map[string]tftypes.Type{
+			"test_attribute": tftypes.String,
+		},
+	}
+
+	testProto6Value := tftypes.NewValue(testProto6Type, map[string]tftypes.Value{
+		"test_attribute": tftypes.NewValue(tftypes.String, "test-value"),
+	})
+
+	testProto6DynamicValue, err := tfprotov6.NewDynamicValue(testProto6Type, testProto6Value)
+
+	if err != nil {
+		t.Fatalf("unexpected error calling tfprotov6.NewDynamicValue(): %s", err)
+	}
+
+	testState := &tfsdk.State{
+		Raw: testProto6Value,
+		Schema: tfsdk.Schema{
+			Attributes: map[string]tfsdk.Attribute{
+				"test_attribute": {
+					Required: true,
+					Type:     types.StringType,
+				},
+			},
+		},
+	}
+
+	testStateInvalid := &tfsdk.State{
+		Raw: testProto6Value,
+		Schema: tfsdk.Schema{
+			Attributes: map[string]tfsdk.Attribute{
+				"test_attribute": {
+					Required: true,
+					Type:     types.BoolType,
+				},
+			},
+		},
+	}
+
+	testCases := map[string]struct {
+		input    *fwserver.ReadDataSourceResponse
+		expected *tfprotov6.ReadDataSourceResponse
+	}{
+		"nil": {
+			input:    nil,
+			expected: nil,
+		},
+		"empty": {
+			input:    &fwserver.ReadDataSourceResponse{},
+			expected: &tfprotov6.ReadDataSourceResponse{},
+		},
+		"diagnostics": {
+			input: &fwserver.ReadDataSourceResponse{
+				Diagnostics: diag.Diagnostics{
+					diag.NewWarningDiagnostic("test warning summary", "test warning details"),
+					diag.NewErrorDiagnostic("test error summary", "test error details"),
+				},
+			},
+			expected: &tfprotov6.ReadDataSourceResponse{
+				Diagnostics: []*tfprotov6.Diagnostic{
+					{
+						Severity: tfprotov6.DiagnosticSeverityWarning,
+						Summary:  "test warning summary",
+						Detail:   "test warning details",
+					},
+					{
+						Severity: tfprotov6.DiagnosticSeverityError,
+						Summary:  "test error summary",
+						Detail:   "test error details",
+					},
+				},
+			},
+		},
+		"diagnostics-invalid-state": {
+			input: &fwserver.ReadDataSourceResponse{
+				Diagnostics: diag.Diagnostics{
+					diag.NewWarningDiagnostic("test warning summary", "test warning details"),
+					diag.NewErrorDiagnostic("test error summary", "test error details"),
+				},
+				State: testStateInvalid,
+			},
+			expected: &tfprotov6.ReadDataSourceResponse{
+				Diagnostics: []*tfprotov6.Diagnostic{
+					{
+						Severity: tfprotov6.DiagnosticSeverityWarning,
+						Summary:  "test warning summary",
+						Detail:   "test warning details",
+					},
+					{
+						Severity: tfprotov6.DiagnosticSeverityError,
+						Summary:  "test error summary",
+						Detail:   "test error details",
+					},
+					{
+						Severity: tfprotov6.DiagnosticSeverityError,
+						Summary:  "Unable to Convert State",
+						Detail: "An unexpected error was encountered when converting the state to the protocol type. " +
+							"This is always an issue in the Terraform Provider SDK used to implement the provider and should be reported to the provider developers.\n\n" +
+							"Please report this to the provider developer:\n\n" +
+							"AttributeName(\"test_attribute\"): unexpected value type string, tftypes.Bool values must be of type bool",
+					},
+				},
+			},
+		},
+		"state": {
+			input: &fwserver.ReadDataSourceResponse{
+				State: testState,
+			},
+			expected: &tfprotov6.ReadDataSourceResponse{
+				State: &testProto6DynamicValue,
+			},
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := toproto6.ReadDataSourceResponse(context.Background(), testCase.input)
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}

60ebb62ea43cfcf4f04a574996f53e495c9530d6
Brian Flad <bflad417@gmail.com>
2022-05-27 11:06:21 -0400

    internal: Implement fwserver ConfigureProvider testing and update proto6server testing (#352)
    Reference: https://github.com/hashicorp/terraform-plugin-framework/issues/215

--- internal/fwserver/server_configureprovider_test.go
+++ internal/fwserver/server_configureprovider_test.go
@@ -5,21 +5,40 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/internal/fwserver"
-	"github.com/hashicorp/terraform-plugin-framework/internal/testing/emptyprovider"
+	"github.com/hashicorp/terraform-plugin-framework/internal/testing/testprovider"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
 )
 
-// TODO: Migrate tfsdk.Provider bits of proto6server.testProviderServer to
-// new internal/testing/provider.Provider that allows customization of all
-// method implementations via struct fields. Then, create additional test
-// cases in this unit test.
-//
-// For now this testing is covered by proto6server.ConfigureProvider.
-//
-// Reference: https://github.com/hashicorp/terraform-plugin-framework/issues/215
 func TestServerConfigureProvider(t *testing.T) {
 	t.Parallel()
+
+	testType := tftypes.Object{
+		AttributeTypes: map[string]tftypes.Type{
+			"test": tftypes.String,
+		},
+	}
+
+	testValue := tftypes.NewValue(testType, map[string]tftypes.Value{
+		"test": tftypes.NewValue(tftypes.String, "test-value"),
+	})
+
+	testSchema := tfsdk.Schema{
+		Attributes: map[string]tfsdk.Attribute{
+			"test": {
+				Required: true,
+				Type:     types.StringType,
+			},
+		},
+	}
+
+	testConfig := tfsdk.Config{
+		Raw:    testValue,
+		Schema: testSchema,
+	}
 
 	testCases := map[string]struct {
 		server           *fwserver.Server
@@ -28,9 +47,79 @@ func TestServerConfigureProvider(t *testing.T) {
 	}{
 		"empty-provider": {
 			server: &fwserver.Server{
-				Provider: &emptyprovider.Provider{},
+				Provider: &testprovider.Provider{},
 			},
 			expectedResponse: &tfsdk.ConfigureProviderResponse{},
+		},
+		"request-config": {
+			server: &fwserver.Server{
+				Provider: &testprovider.Provider{
+					GetSchemaMethod: func(_ context.Context) (tfsdk.Schema, diag.Diagnostics) {
+						return testSchema, nil
+					},
+					ConfigureMethod: func(ctx context.Context, req tfsdk.ConfigureProviderRequest, resp *tfsdk.ConfigureProviderResponse) {
+						var got types.String
+
+						resp.Diagnostics.Append(req.Config.GetAttribute(ctx, tftypes.NewAttributePath().WithAttributeName("test"), &got)...)
+
+						if resp.Diagnostics.HasError() {
+							return
+						}
+
+						if got.Value != "test-value" {
+							resp.Diagnostics.AddError("Incorrect req.Config", "expected test-value, got "+got.Value)
+						}
+					},
+				},
+			},
+			request: &tfsdk.ConfigureProviderRequest{
+				Config: testConfig,
+			},
+			expectedResponse: &tfsdk.ConfigureProviderResponse{},
+		},
+		"request-terraformversion": {
+			server: &fwserver.Server{
+				Provider: &testprovider.Provider{
+					GetSchemaMethod: func(_ context.Context) (tfsdk.Schema, diag.Diagnostics) {
+						return tfsdk.Schema{}, nil
+					},
+					ConfigureMethod: func(ctx context.Context, req tfsdk.ConfigureProviderRequest, resp *tfsdk.ConfigureProviderResponse) {
+						if req.TerraformVersion != "1.0.0" {
+							resp.Diagnostics.AddError("Incorrect req.TerraformVersion", "expected 1.0.0, got "+req.TerraformVersion)
+						}
+					},
+				},
+			},
+			request: &tfsdk.ConfigureProviderRequest{
+				TerraformVersion: "1.0.0",
+			},
+			expectedResponse: &tfsdk.ConfigureProviderResponse{},
+		},
+		"response-diagnostics": {
+			server: &fwserver.Server{
+				Provider: &testprovider.Provider{
+					GetSchemaMethod: func(_ context.Context) (tfsdk.Schema, diag.Diagnostics) {
+						return tfsdk.Schema{}, nil
+					},
+					ConfigureMethod: func(ctx context.Context, req tfsdk.ConfigureProviderRequest, resp *tfsdk.ConfigureProviderResponse) {
+						resp.Diagnostics.AddWarning("warning summary", "warning detail")
+						resp.Diagnostics.AddError("error summary", "error detail")
+					},
+				},
+			},
+			request: &tfsdk.ConfigureProviderRequest{},
+			expectedResponse: &tfsdk.ConfigureProviderResponse{
+				Diagnostics: diag.Diagnostics{
+					diag.NewWarningDiagnostic(
+						"warning summary",
+						"warning detail",
+					),
+					diag.NewErrorDiagnostic(
+						"error summary",
+						"error detail",
+					),
+				},
+			},
 		},
 	}
 

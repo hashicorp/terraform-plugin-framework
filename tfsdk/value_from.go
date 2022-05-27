1600e23c456fb3130c3604edb4f631f24d8b01fe
Ivan De Marino <ivan.demarino@hashicorp.com>
2022-05-27 17:00:15 +0100

    Allowing `ValueFrom` to set the resulting converted value on any compatible pointer provided as `target`

--- tfsdk/value_from.go
+++ tfsdk/value_from.go
@@ -2,21 +2,22 @@ package tfsdk
 
 import (
 	"context"
+
 	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/internal/reflect"
 	"github.com/hashicorp/terraform-plugin-go/tftypes"
 )
 
-// ValueFrom populates the attr value passed as `val` with
-// the contents of `target`, using the reflection rules
-// defined for `Get` and `GetAttribute`.
-func ValueFrom(ctx context.Context, typ attr.Type, val *attr.Value, target interface{}) diag.Diagnostics {
-	v, diags := reflect.FromValue(ctx, typ, target, tftypes.NewAttributePath())
+// ValueFrom takes the Go value `val` and populates `target` with an attr.Value,
+// based on the type definition provided in `targetType`.
+//
+// This is achieved using reflection rules provided by the internal/reflect package.
+func ValueFrom(ctx context.Context, val interface{}, targetType attr.Type, target interface{}) diag.Diagnostics {
+	v, diags := reflect.FromValue(ctx, targetType, val, tftypes.NewAttributePath())
 	if diags.HasError() {
 		return diags
 	}
 
-	*val = v
-	return diags
+	return ValueAs(ctx, v, target)
 }
--- tfsdk/value_from_test.go
+++ tfsdk/value_from_test.go
@@ -8,12 +8,14 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
 )
 
 type person struct {
 	Name    types.String `tfsdk:"name"`
 	Age     types.Int64  `tfsdk:"age"`
 	OptedIn types.Bool   `tfsdk:"opted_in"`
+	Address types.List   `tfsdk:"address"`
 }
 
 func TestValueFrom(t *testing.T) {
@@ -23,41 +25,76 @@ func TestValueFrom(t *testing.T) {
 		"name":     types.StringType,
 		"age":      types.Int64Type,
 		"opted_in": types.BoolType,
+		"address": types.ListType{
+			ElemType: types.StringType,
+		},
 	}
 
-	x := person{
+	mrX := person{
 		Name:    types.String{Value: "x"},
 		Age:     types.Int64{Value: 30},
 		OptedIn: types.Bool{Value: true},
+		Address: types.List{
+			ElemType: types.StringType,
+			Elems: []attr.Value{
+				types.String{Value: "1"},
+				types.String{Value: "Beckford Close"},
+				types.String{Value: "Gotham"},
+			},
+		},
 	}
 
-	y := person{
+	mrsY := person{
 		Name:    types.String{Value: "y"},
 		Age:     types.Int64{Value: 23},
 		OptedIn: types.Bool{Value: false},
+		Address: types.List{
+			ElemType: types.StringType,
+			Elems: []attr.Value{
+				types.String{Value: "2"},
+				types.String{Value: "Windmill Close"},
+				types.String{Value: "Smallville"},
+			},
+		},
 	}
 
-	xObj := types.Object{
+	expectedMrXObj := types.Object{
 		AttrTypes: personAttrTypes,
 		Attrs: map[string]attr.Value{
 			"name":     types.String{Value: "x", Unknown: false, Null: false},
 			"age":      types.Int64{Value: 30, Unknown: false, Null: false},
 			"opted_in": types.Bool{Value: true, Unknown: false, Null: false},
+			"address": types.List{
+				ElemType: types.StringType,
+				Elems: []attr.Value{
+					types.String{Value: "1"},
+					types.String{Value: "Beckford Close"},
+					types.String{Value: "Gotham"},
+				},
+			},
 		},
 	}
 
-	yObj := types.Object{
+	expectedMrsYObj := types.Object{
 		AttrTypes: personAttrTypes,
 		Attrs: map[string]attr.Value{
 			"name":     types.String{Value: "y", Unknown: false, Null: false},
 			"age":      types.Int64{Value: 23, Unknown: false, Null: false},
 			"opted_in": types.Bool{Value: false, Unknown: false, Null: false},
+			"address": types.List{
+				ElemType: types.StringType,
+				Elems: []attr.Value{
+					types.String{Value: "2"},
+					types.String{Value: "Windmill Close"},
+					types.String{Value: "Smallville"},
+				},
+			},
 		},
 	}
 
 	type testCase struct {
-		target        attr.Value
 		val           interface{}
+		target        attr.Value
 		expected      attr.Value
 		expectedDiags diag.Diagnostics
 	}
@@ -69,14 +106,14 @@ func TestValueFrom(t *testing.T) {
 			expected: types.String{Value: "hello", Unknown: false, Null: false},
 		},
 		"struct": {
-			val: x,
+			val: mrX,
 			target: types.Object{
 				AttrTypes: personAttrTypes,
 			},
-			expected: xObj,
+			expected: expectedMrXObj,
 		},
 		"list": {
-			val: []person{x, y},
+			val: []person{mrX, mrsY},
 			target: types.List{
 				ElemType: types.ObjectType{
 					AttrTypes: personAttrTypes,
@@ -86,23 +123,22 @@ func TestValueFrom(t *testing.T) {
 				ElemType: types.ObjectType{
 					AttrTypes: personAttrTypes,
 				},
-				Elems: []attr.Value{xObj, yObj},
+				Elems: []attr.Value{expectedMrXObj, expectedMrsYObj},
 			},
 		},
-		//"incompatible-type": {
-		//	val:    0,
-		//	target: types.String{},
-		//	expectedDiags: diag.Diagnostics{
-		//		diag.WithPath(
-		//			tftypes.NewAttributePath(),
-		//			reflect.DiagIntoIncompatibleType{
-		//				Val:        tftypes.NewValue(tftypes.String, ""),
-		//				TargetType: goreflect.TypeOf(int64(0)),
-		//				Err:        fmt.Errorf("unexpected error was encountered trying to convert from value"),
-		//			},
-		//		),
-		//	},
-		//},
+		"incompatible-type": {
+			val:    0,
+			target: types.String{},
+			expectedDiags: diag.Diagnostics{
+				diag.WithPath(
+					tftypes.NewAttributePath(),
+					diag.NewErrorDiagnostic(
+						"Value Conversion Error",
+						"An unexpected error was encountered trying to convert the Terraform value. This is always an error in the provider. Please report the following to the provider developer:\n\ncan't unmarshal tftypes.Number into *string, expected string",
+					),
+				),
+			},
+		},
 	}
 
 	for name, tc := range tests {
@@ -110,7 +146,7 @@ func TestValueFrom(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 
-			diags := ValueFrom(context.Background(), tc.target.Type(context.Background()), &tc.target, &tc.val)
+			diags := ValueFrom(context.Background(), tc.val, tc.target.Type(context.Background()), &tc.target)
 
 			if diff := cmp.Diff(tc.expectedDiags, diags); diff != "" {
 				t.Fatalf("Unexpected diff in diagnostics (-wanted, +got): %s", diff)

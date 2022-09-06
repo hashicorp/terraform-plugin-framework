a5593d45c8c8b65e64e844efec48bd9b435bf069
Brian Flad <bflad417@gmail.com>
2022-09-06 13:17:17 -0400

    internal/fwserver: Delay deprecated attribute/block warnings for unknown values (#465)
    Reference: https://github.com/hashicorp/terraform-plugin-sdk/pull/1047
    Reference: https://github.com/hashicorp/terraform/issues/31730
    
    This change is made to keep terraform-plugin-sdk and terraform-plugin-framework behaviors for deprecation warning handling in sync.

--- internal/fwserver/block_validation.go
+++ internal/fwserver/block_validation.go
@@ -195,26 +195,13 @@ func BlockValidate(ctx context.Context, b fwschema.Block, req tfsdk.ValidateAttr
 		return
 	}
 
-	if b.GetDeprecationMessage() != "" && attributeConfig != nil {
-		tfValue, err := attributeConfig.ToTerraformValue(ctx)
-
-		if err != nil {
-			resp.Diagnostics.AddAttributeError(
-				req.AttributePath,
-				"Block Validation Error",
-				"Block validation cannot convert value. Report this to the provider developer:\n\n"+err.Error(),
-			)
-
-			return
-		}
-
-		if !tfValue.IsNull() {
-			resp.Diagnostics.AddAttributeWarning(
-				req.AttributePath,
-				"Block Deprecated",
-				b.GetDeprecationMessage(),
-			)
-		}
+	// Show deprecation warning only on known values.
+	if b.GetDeprecationMessage() != "" && !attributeConfig.IsNull() && !attributeConfig.IsUnknown() {
+		resp.Diagnostics.AddAttributeWarning(
+			req.AttributePath,
+			"Block Deprecated",
+			b.GetDeprecationMessage(),
+		)
 	}
 }
 

013e3c55bfb795b3cd262f4d0a4c9e628ce38dbf
Liam Cervante <liam.cervante@hashicorp.com>
2022-09-14 10:56:40 +0200

    update changelog and add unit test

--- internal/fwserver/block_plan_modification_test.go
+++ internal/fwserver/block_plan_modification_test.go
@@ -836,6 +836,173 @@ func TestBlockModifyPlan(t *testing.T) {
 				Private: testProviderData,
 			},
 		},
+		"block-list-nested-block-list": {
+			block: tfsdk.Block{
+				Attributes: map[string]tfsdk.Attribute{
+					"id": {
+						Type:     types.StringType,
+						Computed: true,
+						Optional: true,
+						PlanModifiers: []tfsdk.AttributePlanModifier{
+							resource.UseStateForUnknown(),
+						},
+					},
+				},
+				Blocks: map[string]tfsdk.Block{
+					"list": {
+						Attributes: map[string]tfsdk.Attribute{
+							"id": {
+								Type:     types.StringType,
+								Computed: true,
+								PlanModifiers: []tfsdk.AttributePlanModifier{
+									resource.UseStateForUnknown(),
+								},
+							},
+						},
+						NestingMode: tfsdk.BlockNestingModeList,
+					},
+				},
+				NestingMode: tfsdk.BlockNestingModeList,
+			},
+			req: tfsdk.ModifyAttributePlanRequest{
+				AttributeConfig: types.List{
+					ElemType: types.ObjectType{
+						AttrTypes: map[string]attr.Type{
+							"id": types.StringType,
+							"list": types.ListType{
+								ElemType: types.ObjectType{
+									AttrTypes: map[string]attr.Type{
+										"id": types.StringType,
+									},
+								},
+							},
+						},
+					},
+					Elems: []attr.Value{
+						types.Object{
+							AttrTypes: map[string]attr.Type{
+								"id": types.StringType,
+								"list": types.ListType{
+									ElemType: types.ObjectType{
+										AttrTypes: map[string]attr.Type{
+											"id": types.StringType,
+										},
+									},
+								},
+							},
+							Attrs: map[string]attr.Value{
+								"id": types.String{Value: "one"},
+								"list": types.List{
+									Elems: []attr.Value{},
+									ElemType: types.ObjectType{
+										AttrTypes: map[string]attr.Type{
+											"id": types.StringType,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				AttributePath: path.Root("test"),
+				AttributePlan: types.List{
+					ElemType: types.ObjectType{
+						AttrTypes: map[string]attr.Type{
+							"id": types.StringType,
+							"list": types.ListType{
+								ElemType: types.ObjectType{
+									AttrTypes: map[string]attr.Type{
+										"id": types.StringType,
+									},
+								},
+							},
+						},
+					},
+					Elems: []attr.Value{
+						types.Object{
+							AttrTypes: map[string]attr.Type{
+								"id": types.StringType,
+								"list": types.ListType{
+									ElemType: types.ObjectType{
+										AttrTypes: map[string]attr.Type{
+											"id": types.StringType,
+										},
+									},
+								},
+							},
+							Attrs: map[string]attr.Value{
+								"id": types.String{Value: "one"},
+								"list": types.List{
+									Elems: []attr.Value{},
+									ElemType: types.ObjectType{
+										AttrTypes: map[string]attr.Type{
+											"id": types.StringType,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				AttributeState: types.List{
+					ElemType: types.ObjectType{
+						AttrTypes: map[string]attr.Type{
+							"id": types.StringType,
+							"list": types.ListType{
+								ElemType: types.ObjectType{
+									AttrTypes: map[string]attr.Type{
+										"id": types.StringType,
+									},
+								},
+							},
+						},
+					},
+					Elems: nil,
+				},
+			},
+			expectedResp: ModifyAttributePlanResponse{
+				AttributePlan: types.List{
+					ElemType: types.ObjectType{
+						AttrTypes: map[string]attr.Type{
+							"id": types.StringType,
+							"list": types.ListType{
+								ElemType: types.ObjectType{
+									AttrTypes: map[string]attr.Type{
+										"id": types.StringType,
+									},
+								},
+							},
+						},
+					},
+					Elems: []attr.Value{
+						types.Object{
+							AttrTypes: map[string]attr.Type{
+								"id": types.StringType,
+								"list": types.ListType{
+									ElemType: types.ObjectType{
+										AttrTypes: map[string]attr.Type{
+											"id": types.StringType,
+										},
+									},
+								},
+							},
+							Attrs: map[string]attr.Value{
+								"id": types.String{Value: "one"},
+								"list": types.List{
+									Elems: []attr.Value{},
+									ElemType: types.ObjectType{
+										AttrTypes: map[string]attr.Type{
+											"id": types.StringType,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				Private: testEmptyProviderData,
+			},
+		},
 		"block-set-nested-usestateforunknown": {
 			block: tfsdk.Block{
 				Attributes: map[string]tfsdk.Attribute{

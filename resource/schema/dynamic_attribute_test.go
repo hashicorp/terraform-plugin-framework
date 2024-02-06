2fb02896dfa42e40d4863932d47113fb5ef13305
Austin Valle <austinvalle@gmail.com>
2024-02-06 17:13:21 -0500

    add resource schema attribute test

---
+++ resource/schema/dynamic_attribute_test.go
@@ -0,0 +1,578 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package schema_test
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/internal/fwschema"
+	"github.com/hashicorp/terraform-plugin-framework/internal/testing/testschema"
+	"github.com/hashicorp/terraform-plugin-framework/internal/testing/testtypes"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/defaults"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/dynamicdefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+func TestDynamicAttributeApplyTerraform5AttributePathStep(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute     schema.DynamicAttribute
+		step          tftypes.AttributePathStep
+		expected      any
+		expectedError error
+	}{
+		"AttributeName": {
+			attribute:     schema.DynamicAttribute{},
+			step:          tftypes.AttributeName("test"),
+			expected:      nil,
+			expectedError: fmt.Errorf("cannot apply AttributePathStep tftypes.AttributeName to basetypes.DynamicType"),
+		},
+		"ElementKeyInt": {
+			attribute:     schema.DynamicAttribute{},
+			step:          tftypes.ElementKeyInt(1),
+			expected:      nil,
+			expectedError: fmt.Errorf("cannot apply AttributePathStep tftypes.ElementKeyInt to basetypes.DynamicType"),
+		},
+		"ElementKeyString": {
+			attribute:     schema.DynamicAttribute{},
+			step:          tftypes.ElementKeyString("test"),
+			expected:      nil,
+			expectedError: fmt.Errorf("cannot apply AttributePathStep tftypes.ElementKeyString to basetypes.DynamicType"),
+		},
+		"ElementKeyValue": {
+			attribute:     schema.DynamicAttribute{},
+			step:          tftypes.ElementKeyValue(tftypes.NewValue(tftypes.String, "test")),
+			expected:      nil,
+			expectedError: fmt.Errorf("cannot apply AttributePathStep tftypes.ElementKeyValue to basetypes.DynamicType"),
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got, err := testCase.attribute.ApplyTerraform5AttributePathStep(testCase.step)
+
+			if err != nil {
+				if testCase.expectedError == nil {
+					t.Fatalf("expected no error, got: %s", err)
+				}
+
+				if !strings.Contains(err.Error(), testCase.expectedError.Error()) {
+					t.Fatalf("expected error %q, got: %s", testCase.expectedError, err)
+				}
+			}
+
+			if err == nil && testCase.expectedError != nil {
+				t.Fatalf("got no error, expected: %s", testCase.expectedError)
+			}
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestDynamicAttributeGetDeprecationMessage(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.DynamicAttribute
+		expected  string
+	}{
+		"no-deprecation-message": {
+			attribute: schema.DynamicAttribute{},
+			expected:  "",
+		},
+		"deprecation-message": {
+			attribute: schema.DynamicAttribute{
+				DeprecationMessage: "test deprecation message",
+			},
+			expected: "test deprecation message",
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.GetDeprecationMessage()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestDynamicAttributeEqual(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.DynamicAttribute
+		other     fwschema.Attribute
+		expected  bool
+	}{
+		"different-type": {
+			attribute: schema.DynamicAttribute{},
+			other:     testschema.AttributeWithDynamicValidators{},
+			expected:  false,
+		},
+		"equal": {
+			attribute: schema.DynamicAttribute{},
+			other:     schema.DynamicAttribute{},
+			expected:  true,
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.Equal(testCase.other)
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestDynamicAttributeGetDescription(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.DynamicAttribute
+		expected  string
+	}{
+		"no-description": {
+			attribute: schema.DynamicAttribute{},
+			expected:  "",
+		},
+		"description": {
+			attribute: schema.DynamicAttribute{
+				Description: "test description",
+			},
+			expected: "test description",
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.GetDescription()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestDynamicAttributeGetMarkdownDescription(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.DynamicAttribute
+		expected  string
+	}{
+		"no-markdown-description": {
+			attribute: schema.DynamicAttribute{},
+			expected:  "",
+		},
+		"markdown-description": {
+			attribute: schema.DynamicAttribute{
+				MarkdownDescription: "test description",
+			},
+			expected: "test description",
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.GetMarkdownDescription()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestDynamicAttributeGetType(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.DynamicAttribute
+		expected  attr.Type
+	}{
+		"base": {
+			attribute: schema.DynamicAttribute{},
+			expected:  types.DynamicType,
+		},
+		"custom-type": {
+			attribute: schema.DynamicAttribute{
+				CustomType: testtypes.DynamicType{},
+			},
+			expected: testtypes.DynamicType{},
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.GetType()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestDynamicAttributeIsComputed(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.DynamicAttribute
+		expected  bool
+	}{
+		"not-computed": {
+			attribute: schema.DynamicAttribute{},
+			expected:  false,
+		},
+		"computed": {
+			attribute: schema.DynamicAttribute{
+				Computed: true,
+			},
+			expected: true,
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.IsComputed()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestDynamicAttributeIsOptional(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.DynamicAttribute
+		expected  bool
+	}{
+		"not-optional": {
+			attribute: schema.DynamicAttribute{},
+			expected:  false,
+		},
+		"optional": {
+			attribute: schema.DynamicAttribute{
+				Optional: true,
+			},
+			expected: true,
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.IsOptional()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestDynamicAttributeIsRequired(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.DynamicAttribute
+		expected  bool
+	}{
+		"not-required": {
+			attribute: schema.DynamicAttribute{},
+			expected:  false,
+		},
+		"required": {
+			attribute: schema.DynamicAttribute{
+				Required: true,
+			},
+			expected: true,
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.IsRequired()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestDynamicAttributeIsSensitive(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.DynamicAttribute
+		expected  bool
+	}{
+		"not-sensitive": {
+			attribute: schema.DynamicAttribute{},
+			expected:  false,
+		},
+		"sensitive": {
+			attribute: schema.DynamicAttribute{
+				Sensitive: true,
+			},
+			expected: true,
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.IsSensitive()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestDynamicAttributeDynamicDefaultValue(t *testing.T) {
+	t.Parallel()
+
+	opt := cmp.Comparer(func(x, y defaults.Dynamic) bool {
+		ctx := context.Background()
+		req := defaults.DynamicRequest{}
+
+		xResp := defaults.DynamicResponse{}
+		x.DefaultDynamic(ctx, req, &xResp)
+
+		yResp := defaults.DynamicResponse{}
+		y.DefaultDynamic(ctx, req, &yResp)
+
+		return xResp.PlanValue.Equal(yResp.PlanValue)
+	})
+
+	testCases := map[string]struct {
+		attribute schema.DynamicAttribute
+		expected  defaults.Dynamic
+	}{
+		"no-default": {
+			attribute: schema.DynamicAttribute{},
+			expected:  nil,
+		},
+		"default": {
+			attribute: schema.DynamicAttribute{
+				Default: dynamicdefault.StaticDynamic(types.DynamicValue(types.StringValue("test-value"))),
+			},
+			expected: dynamicdefault.StaticDynamic(types.DynamicValue(types.StringValue("test-value"))),
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.DynamicDefaultValue()
+
+			if diff := cmp.Diff(got, testCase.expected, opt); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestDynamicAttributeDynamicPlanModifiers(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.DynamicAttribute
+		expected  []planmodifier.Dynamic
+	}{
+		"no-planmodifiers": {
+			attribute: schema.DynamicAttribute{},
+			expected:  nil,
+		},
+		"planmodifiers": {
+			attribute: schema.DynamicAttribute{
+				PlanModifiers: []planmodifier.Dynamic{},
+			},
+			expected: []planmodifier.Dynamic{},
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.DynamicPlanModifiers()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestDynamicAttributeDynamicValidators(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.DynamicAttribute
+		expected  []validator.Dynamic
+	}{
+		"no-validators": {
+			attribute: schema.DynamicAttribute{},
+			expected:  nil,
+		},
+		"validators": {
+			attribute: schema.DynamicAttribute{
+				Validators: []validator.Dynamic{},
+			},
+			expected: []validator.Dynamic{},
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.DynamicValidators()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestDynamicAttributeValidateImplementation(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.DynamicAttribute
+		request   fwschema.ValidateImplementationRequest
+		expected  *fwschema.ValidateImplementationResponse
+	}{
+		"computed": {
+			attribute: schema.DynamicAttribute{
+				Computed: true,
+			},
+			request: fwschema.ValidateImplementationRequest{
+				Name: "test",
+				Path: path.Root("test"),
+			},
+			expected: &fwschema.ValidateImplementationResponse{},
+		},
+		"default-without-computed": {
+			attribute: schema.DynamicAttribute{
+				Default: dynamicdefault.StaticDynamic(types.DynamicValue(types.StringValue("test"))),
+			},
+			request: fwschema.ValidateImplementationRequest{
+				Name: "test",
+				Path: path.Root("test"),
+			},
+			expected: &fwschema.ValidateImplementationResponse{
+				Diagnostics: diag.Diagnostics{
+					diag.NewErrorDiagnostic(
+						"Schema Using Attribute Default For Non-Computed Attribute",
+						"Attribute \"test\" must be computed when using default. "+
+							"This is an issue with the provider and should be reported to the provider developers.",
+					),
+				},
+			},
+		},
+		"default-with-computed": {
+			attribute: schema.DynamicAttribute{
+				Computed: true,
+				Default:  dynamicdefault.StaticDynamic(types.DynamicValue(types.StringValue("test"))),
+			},
+			request: fwschema.ValidateImplementationRequest{
+				Name: "test",
+				Path: path.Root("test"),
+			},
+			expected: &fwschema.ValidateImplementationResponse{},
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := &fwschema.ValidateImplementationResponse{}
+			testCase.attribute.ValidateImplementation(context.Background(), testCase.request, got)
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}

4f3d9ec1d6ffd843c8e990e57196f01ee2c9f3e7
Austin Valle <austinvalle@gmail.com>
2024-02-06 14:33:06 -0500

    add plan modifier for dynamic

--- resource/schema/planmodifier/dynamic.go
+++ resource/schema/planmodifier/dynamic.go
@@ -13,16 +13,16 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-// Object is a schema plan modifier for types.Object attributes.
-type Object interface {
+// Dynamic is a schema plan modifier for types.Dynamic attributes.
+type Dynamic interface {
 	Describer
 
-	// PlanModifyObject should perform the modification.
-	PlanModifyObject(context.Context, ObjectRequest, *ObjectResponse)
+	// PlanModifyDynamic should perform the modification.
+	PlanModifyDynamic(context.Context, DynamicRequest, *DynamicResponse)
 }
 
-// ObjectRequest is a request for types.Object schema plan modification.
-type ObjectRequest struct {
+// DynamicRequest is a request for types.Dynamic schema plan modification.
+type DynamicRequest struct {
 	// Path contains the path of the attribute for modification. Use this path
 	// for any response diagnostics.
 	Path path.Path
@@ -35,46 +35,46 @@ type ObjectRequest struct {
 	Config tfsdk.Config
 
 	// ConfigValue contains the value of the attribute for modification from the configuration.
-	ConfigValue types.Object
+	ConfigValue types.Dynamic
 
 	// Plan contains the entire proposed new state of the resource.
 	Plan tfsdk.Plan
 
 	// PlanValue contains the value of the attribute for modification from the proposed new state.
-	PlanValue types.Object
+	PlanValue types.Dynamic
 
 	// State contains the entire prior state of the resource.
 	State tfsdk.State
 
 	// StateValue contains the value of the attribute for modification from the prior state.
-	StateValue types.Object
+	StateValue types.Dynamic
 
 	// Private is provider-defined resource private state data which was previously
 	// stored with the resource state. This data is opaque to Terraform and does
 	// not affect plan output. Any existing data is copied to
-	// ObjectResponse.Private to prevent accidental private state data loss.
+	// DynamicResponse.Private to prevent accidental private state data loss.
 	//
 	// The private state data is always the original data when the schema-based plan
 	// modification began or, is updated as the logic traverses deeper into underlying
 	// attributes.
 	//
 	// Use the GetKey method to read data. Use the SetKey method on
-	// ObjectResponse.Private to update or remove a value.
+	// DynamicResponse.Private to update or remove a value.
 	Private *privatestate.ProviderData
 }
 
-// ObjectResponse is a response to a ObjectRequest.
-type ObjectResponse struct {
+// DynamicResponse is a response to a DynamicRequest.
+type DynamicResponse struct {
 	// PlanValue is the planned new state for the attribute.
-	PlanValue types.Object
+	PlanValue types.Dynamic
 
 	// RequiresReplace indicates whether a change in the attribute
 	// requires replacement of the whole resource.
 	RequiresReplace bool
 
-	// Private is the private state resource data following the PlanModifyObject operation.
-	// This field is pre-populated from ObjectRequest.Private and
-	// can be modified during the resource's PlanModifyObject operation.
+	// Private is the private state resource data following the PlanModifyDynamic operation.
+	// This field is pre-populated from DynamicRequest.Private and
+	// can be modified during the resource's PlanModifyDynamic operation.
 	//
 	// The private state data is always the original data when the schema-based plan
 	// modification began or, is updated as the logic traverses deeper into underlying

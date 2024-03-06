07e9a67e2a91f3b70fdf7dc882b7e5622aef1c59
Austin Valle <austinvalle@gmail.com>
2024-03-05 20:50:32 -0500

    add tuple tests for completeness

--- internal/fwschema/static_type_validate_test.go
+++ internal/fwschema/static_type_validate_test.go
@@ -31,6 +31,158 @@ func TestTypeContainsCollectionWithDynamic(t *testing.T) {
 			attrTyp:  types.StringType,
 			expected: false,
 		},
+		"list-missing": {
+			attrTyp:  types.ListType{},
+			expected: false,
+		},
+		"list-static": {
+			attrTyp: types.ListType{
+				ElemType: types.StringType,
+			},
+			expected: false,
+		},
+		"list-list-static": {
+			attrTyp: types.ListType{
+				ElemType: types.ListType{
+					ElemType: types.StringType,
+				},
+			},
+			expected: false,
+		},
+		"list-obj-static": {
+			attrTyp: types.ListType{
+				ElemType: types.ObjectType{
+					AttrTypes: map[string]attr.Type{
+						"bool":    types.BoolType,
+						"float64": types.Float64Type,
+					},
+				},
+			},
+			expected: false,
+		},
+		"list-tuple-static": {
+			attrTyp: types.ListType{
+				ElemType: types.TupleType{
+					ElemTypes: []attr.Type{
+						types.BoolType,
+						types.Float64Type,
+					},
+				},
+			},
+			expected: false,
+		},
+		"list-dynamic": {
+			attrTyp: types.ListType{
+				ElemType: types.DynamicType,
+			},
+			expected: true,
+		},
+		"list-list-dynamic": {
+			attrTyp: types.ListType{
+				ElemType: types.ListType{
+					ElemType: types.DynamicType,
+				},
+			},
+			expected: true,
+		},
+		"list-obj-dynamic": {
+			attrTyp: types.ListType{
+				ElemType: types.ObjectType{
+					AttrTypes: map[string]attr.Type{
+						"bool":    types.BoolType,
+						"dynamic": types.DynamicType,
+					},
+				},
+			},
+			expected: true,
+		},
+		"list-tuple-dynamic": {
+			attrTyp: types.ListType{
+				ElemType: types.TupleType{
+					ElemTypes: []attr.Type{
+						types.BoolType,
+						types.DynamicType,
+					},
+				},
+			},
+			expected: true,
+		},
+		"map-missing": {
+			attrTyp:  types.MapType{},
+			expected: false,
+		},
+		"map-static": {
+			attrTyp: types.MapType{
+				ElemType: types.StringType,
+			},
+			expected: false,
+		},
+		"map-map-static": {
+			attrTyp: types.MapType{
+				ElemType: types.MapType{
+					ElemType: types.StringType,
+				},
+			},
+			expected: false,
+		},
+		"map-obj-static": {
+			attrTyp: types.MapType{
+				ElemType: types.ObjectType{
+					AttrTypes: map[string]attr.Type{
+						"bool":    types.BoolType,
+						"float64": types.Float64Type,
+					},
+				},
+			},
+			expected: false,
+		},
+		"map-tuple-static": {
+			attrTyp: types.MapType{
+				ElemType: types.TupleType{
+					ElemTypes: []attr.Type{
+						types.BoolType,
+						types.Float64Type,
+					},
+				},
+			},
+			expected: false,
+		},
+		"map-dynamic": {
+			attrTyp: types.MapType{
+				ElemType: types.DynamicType,
+			},
+			expected: true,
+		},
+		"map-map-dynamic": {
+			attrTyp: types.MapType{
+				ElemType: types.MapType{
+					ElemType: types.DynamicType,
+				},
+			},
+			expected: true,
+		},
+		"map-obj-dynamic": {
+			attrTyp: types.MapType{
+				ElemType: types.ObjectType{
+					AttrTypes: map[string]attr.Type{
+						"bool":    types.BoolType,
+						"dynamic": types.DynamicType,
+					},
+				},
+			},
+			expected: true,
+		},
+		"map-tuple-dynamic": {
+			attrTyp: types.MapType{
+				ElemType: types.TupleType{
+					ElemTypes: []attr.Type{
+						types.BoolType,
+						types.DynamicType,
+					},
+				},
+			},
+			expected: true,
+		},
 		"obj-list-missing": {
 			attrTyp: types.ObjectType{
 				AttrTypes: map[string]attr.Type{
@@ -367,153 +519,337 @@ func TestTypeContainsCollectionWithDynamic(t *testing.T) {
 			},
 			expected: true,
 		},
-		"list-missing": {
-			attrTyp:  types.ListType{},
-			expected: false,
-		},
-		"list-static": {
-			attrTyp: types.ListType{
-				ElemType: types.StringType,
-			},
-			expected: false,
-		},
-		"list-list-static": {
-			attrTyp: types.ListType{
-				ElemType: types.ListType{
-					ElemType: types.StringType,
+		"tuple-list-missing": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.ListType{},
 				},
 			},
 			expected: false,
 		},
-		"list-obj-static": {
-			attrTyp: types.ListType{
-				ElemType: types.ObjectType{
-					AttrTypes: map[string]attr.Type{
-						"bool":    types.BoolType,
-						"float64": types.Float64Type,
+		"tuple-list-static": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.ListType{
+						ElemType: types.StringType,
 					},
 				},
 			},
 			expected: false,
 		},
-		"list-tuple-static": {
-			attrTyp: types.ListType{
-				ElemType: types.TupleType{
-					ElemTypes: []attr.Type{
-						types.BoolType,
-						types.Float64Type,
+		"tuple-list-list-static": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.ListType{
+						ElemType: types.ListType{
+							ElemType: types.StringType,
+						},
 					},
 				},
 			},
 			expected: false,
 		},
-		"list-dynamic": {
-			attrTyp: types.ListType{
-				ElemType: types.DynamicType,
-			},
-			expected: true,
-		},
-		"list-list-dynamic": {
-			attrTyp: types.ListType{
-				ElemType: types.ListType{
-					ElemType: types.DynamicType,
-				},
-			},
-			expected: true,
-		},
-		"list-obj-dynamic": {
-			attrTyp: types.ListType{
-				ElemType: types.ObjectType{
-					AttrTypes: map[string]attr.Type{
-						"bool":    types.BoolType,
-						"dynamic": types.DynamicType,
-					},
-				},
-			},
-			expected: true,
-		},
-		"list-tuple-dynamic": {
-			attrTyp: types.ListType{
-				ElemType: types.TupleType{
-					ElemTypes: []attr.Type{
-						types.BoolType,
-						types.DynamicType,
-					},
-				},
-			},
-			expected: true,
-		},
-		"map-missing": {
-			attrTyp:  types.MapType{},
-			expected: false,
-		},
-		"map-static": {
-			attrTyp: types.MapType{
-				ElemType: types.StringType,
-			},
-			expected: false,
-		},
-		"map-map-static": {
-			attrTyp: types.MapType{
-				ElemType: types.MapType{
-					ElemType: types.StringType,
-				},
-			},
-			expected: false,
-		},
-		"map-obj-static": {
-			attrTyp: types.MapType{
-				ElemType: types.ObjectType{
-					AttrTypes: map[string]attr.Type{
-						"bool":    types.BoolType,
-						"float64": types.Float64Type,
+		"tuple-list-obj-static": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.ListType{
+						ElemType: types.ObjectType{
+							AttrTypes: map[string]attr.Type{
+								"bool":    types.BoolType,
+								"float64": types.Float64Type,
+							},
+						},
 					},
 				},
 			},
 			expected: false,
 		},
-		"map-tuple-static": {
-			attrTyp: types.MapType{
-				ElemType: types.TupleType{
-					ElemTypes: []attr.Type{
-						types.BoolType,
-						types.Float64Type,
+		"tuple-list-tuple-static": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.ListType{
+						ElemType: types.TupleType{
+							ElemTypes: []attr.Type{
+								types.BoolType,
+								types.Float64Type,
+							},
+						},
 					},
 				},
 			},
 			expected: false,
 		},
-		"map-dynamic": {
-			attrTyp: types.MapType{
-				ElemType: types.DynamicType,
-			},
-			expected: true,
-		},
-		"map-map-dynamic": {
-			attrTyp: types.MapType{
-				ElemType: types.MapType{
-					ElemType: types.DynamicType,
-				},
-			},
-			expected: true,
-		},
-		"map-obj-dynamic": {
-			attrTyp: types.MapType{
-				ElemType: types.ObjectType{
-					AttrTypes: map[string]attr.Type{
-						"bool":    types.BoolType,
-						"dynamic": types.DynamicType,
+		"tuple-list-dynamic": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.ListType{
+						ElemType: types.DynamicType,
 					},
 				},
 			},
 			expected: true,
 		},
-		"map-tuple-dynamic": {
-			attrTyp: types.MapType{
-				ElemType: types.TupleType{
-					ElemTypes: []attr.Type{
-						types.BoolType,
-						types.DynamicType,
+		"tuple-list-list-dynamic": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.ListType{
+						ElemType: types.ListType{
+							ElemType: types.DynamicType,
+						},
+					},
+				},
+			},
+			expected: true,
+		},
+		"tuple-list-obj-dynamic": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.ListType{
+						ElemType: types.ObjectType{
+							AttrTypes: map[string]attr.Type{
+								"bool":    types.BoolType,
+								"dynamic": types.DynamicType,
+							},
+						},
+					},
+				},
+			},
+			expected: true,
+		},
+		"tuple-list-tuple-dynamic": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.ListType{
+						ElemType: types.TupleType{
+							ElemTypes: []attr.Type{
+								types.BoolType,
+								types.DynamicType,
+							},
+						},
+					},
+				},
+			},
+			expected: true,
+		},
+		"tuple-map-missing": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.MapType{},
+				},
+			},
+			expected: false,
+		},
+		"tuple-map-static": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.MapType{
+						ElemType: types.StringType,
+					},
+				},
+			},
+			expected: false,
+		},
+		"tuple-map-map-static": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.MapType{
+						ElemType: types.MapType{
+							ElemType: types.StringType,
+						},
+					},
+				},
+			},
+			expected: false,
+		},
+		"tuple-map-obj-static": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.MapType{
+						ElemType: types.ObjectType{
+							AttrTypes: map[string]attr.Type{
+								"bool":    types.BoolType,
+								"float64": types.Float64Type,
+							},
+						},
+					},
+				},
+			},
+			expected: false,
+		},
+		"tuple-map-tuple-static": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.MapType{
+						ElemType: types.TupleType{
+							ElemTypes: []attr.Type{
+								types.BoolType,
+								types.Float64Type,
+							},
+						},
+					},
+				},
+			},
+			expected: false,
+		},
+		"tuple-map-dynamic": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.MapType{
+						ElemType: types.DynamicType,
+					},
+				},
+			},
+			expected: true,
+		},
+		"tuple-map-map-dynamic": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.MapType{
+						ElemType: types.MapType{
+							ElemType: types.DynamicType,
+						},
+					},
+				},
+			},
+			expected: true,
+		},
+		"tuple-map-obj-dynamic": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.MapType{
+						ElemType: types.ObjectType{
+							AttrTypes: map[string]attr.Type{
+								"bool":    types.BoolType,
+								"dynamic": types.DynamicType,
+							},
+						},
+					},
+				},
+			},
+			expected: true,
+		},
+		"tuple-map-tuple-dynamic": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.MapType{
+						ElemType: types.TupleType{
+							ElemTypes: []attr.Type{
+								types.BoolType,
+								types.DynamicType,
+							},
+						},
+					},
+				},
+			},
+			expected: true,
+		},
+		"tuple-set-missing": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.SetType{},
+				},
+			},
+			expected: false,
+		},
+		"tuple-set-static": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.SetType{
+						ElemType: types.StringType,
+					},
+				},
+			},
+			expected: false,
+		},
+		"tuple-set-set-static": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.SetType{
+						ElemType: types.SetType{
+							ElemType: types.StringType,
+						},
+					},
+				},
+			},
+			expected: false,
+		},
+		"tuple-set-obj-static": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.SetType{
+						ElemType: types.ObjectType{
+							AttrTypes: map[string]attr.Type{
+								"bool":    types.BoolType,
+								"float64": types.Float64Type,
+							},
+						},
+					},
+				},
+			},
+			expected: false,
+		},
+		"tuple-set-tuple-static": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.SetType{
+						ElemType: types.TupleType{
+							ElemTypes: []attr.Type{
+								types.BoolType,
+								types.Float64Type,
+							},
+						},
+					},
+				},
+			},
+			expected: false,
+		},
+		"tuple-set-dynamic": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.SetType{
+						ElemType: types.DynamicType,
+					},
+				},
+			},
+			expected: true,
+		},
+		"tuple-set-set-dynamic": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.SetType{
+						ElemType: types.SetType{
+							ElemType: types.DynamicType,
+						},
+					},
+				},
+			},
+			expected: true,
+		},
+		"tuple-set-obj-dynamic": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.SetType{
+						ElemType: types.ObjectType{
+							AttrTypes: map[string]attr.Type{
+								"bool":    types.BoolType,
+								"dynamic": types.DynamicType,
+							},
+						},
+					},
+				},
+			},
+			expected: true,
+		},
+		"tuple-set-tuple-dynamic": {
+			attrTyp: types.TupleType{
+				ElemTypes: []attr.Type{
+					types.SetType{
+						ElemType: types.TupleType{
+							ElemTypes: []attr.Type{
+								types.BoolType,
+								types.DynamicType,
+							},
+						},
 					},
 				},
 			},

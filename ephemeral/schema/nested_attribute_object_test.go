9b81285a8dfb1195baf1b99875bfda9e4c2f8e01
Austin Valle <austinvalle@gmail.com>
2024-08-13 15:37:11 -0400

    add nested attribute implementations

---
+++ ephemeral/schema/nested_attribute_object_test.go
@@ -0,0 +1,280 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package schema_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/ephemeral/schema"
+	"github.com/hashicorp/terraform-plugin-framework/internal/fwschema"
+	"github.com/hashicorp/terraform-plugin-framework/internal/testing/testtypes"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+)
+
+func TestNestedAttributeObjectApplyTerraform5AttributePathStep(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		object        schema.NestedAttributeObject
+		step          tftypes.AttributePathStep
+		expected      any
+		expectedError error
+	}{
+		"AttributeName": {
+			object: schema.NestedAttributeObject{
+				Attributes: map[string]schema.Attribute{
+					"testattr": schema.StringAttribute{},
+				},
+			},
+			step:          tftypes.AttributeName("testattr"),
+			expected:      schema.StringAttribute{},
+			expectedError: nil,
+		},
+		"AttributeName-missing": {
+			object: schema.NestedAttributeObject{
+				Attributes: map[string]schema.Attribute{
+					"testattr": schema.StringAttribute{},
+				},
+			},
+			step:          tftypes.AttributeName("other"),
+			expected:      nil,
+			expectedError: fmt.Errorf("no attribute \"other\" on NestedAttributeObject"),
+		},
+		"ElementKeyInt": {
+			object: schema.NestedAttributeObject{
+				Attributes: map[string]schema.Attribute{
+					"testattr": schema.StringAttribute{},
+				},
+			},
+			step:          tftypes.ElementKeyInt(1),
+			expected:      nil,
+			expectedError: fmt.Errorf("cannot apply AttributePathStep tftypes.ElementKeyInt to NestedAttributeObject"),
+		},
+		"ElementKeyString": {
+			object: schema.NestedAttributeObject{
+				Attributes: map[string]schema.Attribute{
+					"testattr": schema.StringAttribute{},
+				},
+			},
+			step:          tftypes.ElementKeyString("test"),
+			expected:      nil,
+			expectedError: fmt.Errorf("cannot apply AttributePathStep tftypes.ElementKeyString to NestedAttributeObject"),
+		},
+		"ElementKeyValue": {
+			object: schema.NestedAttributeObject{
+				Attributes: map[string]schema.Attribute{
+					"testattr": schema.StringAttribute{},
+				},
+			},
+			step:          tftypes.ElementKeyValue(tftypes.NewValue(tftypes.String, "test")),
+			expected:      nil,
+			expectedError: fmt.Errorf("cannot apply AttributePathStep tftypes.ElementKeyValue to NestedAttributeObject"),
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got, err := testCase.object.ApplyTerraform5AttributePathStep(testCase.step)
+
+			if err != nil {
+				if testCase.expectedError == nil {
+					t.Fatalf("expected no error, got: %s", err)
+				}
+
+				if !strings.Contains(err.Error(), testCase.expectedError.Error()) {
+					t.Fatalf("expected error %q, got: %s", testCase.expectedError, err)
+				}
+			}
+
+			if err == nil && testCase.expectedError != nil {
+				t.Fatalf("got no error, expected: %s", testCase.expectedError)
+			}
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestNestedAttributeObjectEqual(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		object   schema.NestedAttributeObject
+		other    fwschema.NestedAttributeObject
+		expected bool
+	}{
+		"different-attributes": {
+			object: schema.NestedAttributeObject{
+				Attributes: map[string]schema.Attribute{
+					"testattr": schema.StringAttribute{},
+				},
+			},
+			other: schema.NestedAttributeObject{
+				Attributes: map[string]schema.Attribute{
+					"testattr": schema.BoolAttribute{},
+				},
+			},
+			expected: false,
+		},
+		"equal": {
+			object: schema.NestedAttributeObject{
+				Attributes: map[string]schema.Attribute{
+					"testattr": schema.StringAttribute{},
+				},
+			},
+			other: schema.NestedAttributeObject{
+				Attributes: map[string]schema.Attribute{
+					"testattr": schema.StringAttribute{},
+				},
+			},
+			expected: true,
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.object.Equal(testCase.other)
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestNestedAttributeObjectGetAttributes(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		object   schema.NestedAttributeObject
+		expected fwschema.UnderlyingAttributes
+	}{
+		"no-attributes": {
+			object:   schema.NestedAttributeObject{},
+			expected: fwschema.UnderlyingAttributes{},
+		},
+		"attributes": {
+			object: schema.NestedAttributeObject{
+				Attributes: map[string]schema.Attribute{
+					"testattr1": schema.StringAttribute{},
+					"testattr2": schema.StringAttribute{},
+				},
+			},
+			expected: fwschema.UnderlyingAttributes{
+				"testattr1": schema.StringAttribute{},
+				"testattr2": schema.StringAttribute{},
+			},
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.object.GetAttributes()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestNestedAttributeObjectObjectValidators(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.NestedAttributeObject
+		expected  []validator.Object
+	}{
+		"no-validators": {
+			attribute: schema.NestedAttributeObject{
+				Attributes: map[string]schema.Attribute{
+					"testattr": schema.StringAttribute{},
+				},
+			},
+			expected: nil,
+		},
+		"validators": {
+			attribute: schema.NestedAttributeObject{
+				Validators: []validator.Object{},
+			},
+			expected: []validator.Object{},
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.ObjectValidators()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestNestedAttributeObjectType(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		object   schema.NestedAttributeObject
+		expected attr.Type
+	}{
+		"base": {
+			object: schema.NestedAttributeObject{
+				Attributes: map[string]schema.Attribute{
+					"testattr": schema.StringAttribute{},
+				},
+			},
+			expected: types.ObjectType{
+				AttrTypes: map[string]attr.Type{
+					"testattr": types.StringType,
+				},
+			},
+		},
+		"custom-type": {
+			object: schema.NestedAttributeObject{
+				CustomType: testtypes.ObjectType{},
+			},
+			expected: testtypes.ObjectType{},
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.object.Type()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}

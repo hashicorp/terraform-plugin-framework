2ba50c1f0f2fce609e3148f4f345f7c1636a3ded
Austin Valle <austinvalle@gmail.com>
2024-08-14 09:33:31 -0400

    fix map docs

--- provider/schema/map_nested_attribute.go
+++ provider/schema/map_nested_attribute.go
@@ -24,7 +24,7 @@ var (
 	_ fwxschema.AttributeWithMapValidators = MapNestedAttribute{}
 )
 
-// MapNestedAttribute represents an attribute that is a set of objects where
+// MapNestedAttribute represents an attribute that is a map of objects where
 // the object attributes can be fully defined, including further nested
 // attributes. When retrieving the value for this attribute, use types.Map
 // as the value type unless the CustomType field is set. The NestedObject field
@@ -34,7 +34,7 @@ var (
 // not require definition beyond type information.
 //
 // Terraform configurations configure this attribute using expressions that
-// return a set of objects or directly via curly brace syntax.
+// return a map of objects or directly via curly brace syntax.
 //
 //	# map of objects
 //	example_attribute = {

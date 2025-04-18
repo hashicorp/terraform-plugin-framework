fc058a85a8ba28610931cf5bbe791dae73e1a97a
Rain Kwan <91649079+rainkwan@users.noreply.github.com>
2025-04-18 16:08:10 -0400

    Update internal/fwserver/server_moveresourcestate.go
    Co-authored-by: Austin Valle <austinvalle@gmail.com>

--- internal/fwserver/server_moveresourcestate.go
+++ internal/fwserver/server_moveresourcestate.go
@@ -246,6 +246,16 @@ func (s *Server) MoveResourceState(ctx context.Context, req *MoveResourceStateRe
 				resp.TargetIdentity = moveStateResp.TargetIdentity
 			}
 
+			if resp.TargetIdentity != nil && req.IdentitySchema == nil {
+				resp.Diagnostics.AddError(
+					"Unexpected Move State Response",
+					"An unexpected error was encountered when creating the move state response. New identity data was returned by the provider move state operation, but the resource does not indicate identity support.\n\n"+
+						"This is always a problem with the provider and should be reported to the provider developer.",
+				)
+
+				return
+			}
+
 			if moveStateResp.TargetPrivate != nil {
 				resp.TargetPrivate.Provider = moveStateResp.TargetPrivate
 			}

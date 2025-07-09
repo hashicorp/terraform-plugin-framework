bf10880d2087259b3efb4aaf92cd783831fdf33a
Austin Valle <austinvalle@gmail.com>
2025-07-09 18:02:09 -0400

    protov5 and fwserver impl

--- internal/fwserver/server.go
+++ internal/fwserver/server.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"sync"
 
+	"github.com/hashicorp/terraform-plugin-framework/action"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/ephemeral"
@@ -39,6 +40,29 @@ type Server struct {
 	// [provider.ConfigureResponse.EphemeralResourceData] field value which is passed
 	// to [ephemeral.ConfigureRequest.ProviderData].
 	EphemeralResourceConfigureData any
+
+	// actionSchemas is the cached Action Schemas for RPCs that need to
+	// convert configuration data from the protocol. If not found, it will be
+	// fetched from the Action.Schema() method.
+	actionSchemas map[string]fwschema.Schema
+
+	// actionSchemasMutex is a mutex to protect concurrent actionSchemas
+	// access from race conditions.
+	actionSchemasMutex sync.RWMutex
+
+	// actionFuncs is the cached Action functions for RPCs that need to
+	// access actions. If not found, it will be fetched from the
+	// Provider.Actions() method.
+	actionFuncs map[string]func() action.Action
+
+	// actionFuncsDiags is the cached Diagnostics obtained while populating
+	// actionFuncs. This is to ensure any warnings or errors are also
+	// returned appropriately when fetching actionFuncs.
+	actionFuncsDiags diag.Diagnostics
+
+	// actionFuncsMutex is a mutex to protect concurrent actionFuncs
+	// access from race conditions.
+	actionFuncsMutex sync.Mutex
 
 	// dataSourceSchemas is the cached DataSource Schemas for RPCs that need to
 	// convert configuration data from the protocol. If not found, it will be
--- internal/proto5server/server_invokeaction.go
+++ internal/proto5server/server_invokeaction.go
@@ -6,11 +6,75 @@ package proto5server
 import (
 	"context"
 
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/internal/fromproto5"
+	"github.com/hashicorp/terraform-plugin-framework/internal/fwserver"
+	"github.com/hashicorp/terraform-plugin-framework/internal/logging"
+	"github.com/hashicorp/terraform-plugin-framework/internal/toproto5"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
 )
 
+// invokeActionErrorDiagnostics returns a value suitable for
+// [InvokeActionServerStream.Events]. It yields a single result that contains
+// the given error diagnostics.
+func invokeActionErrorDiagnostics(ctx context.Context, diags diag.Diagnostics) (*tfprotov5.InvokeActionServerStream, error) {
+	return &tfprotov5.InvokeActionServerStream{
+		Events: func(push func(tfprotov5.InvokeActionEvent) bool) {
+			push(tfprotov5.InvokeActionEvent{
+				Type: tfprotov5.CompletedInvokeActionEventType{
+					Diagnostics: toproto5.Diagnostics(ctx, diags),
+				},
+			})
+		},
+	}, nil
+}
+
 // InvokeAction satisfies the tfprotov5.ProviderServer interface.
 func (s *Server) InvokeAction(ctx context.Context, proto5Req *tfprotov5.InvokeActionRequest) (*tfprotov5.InvokeActionServerStream, error) {
-	// TODO:Actions: Implement
-	panic("unimplemented")
+	ctx = s.registerContext(ctx)
+	ctx = logging.InitContext(ctx)
+
+	fwResp := &fwserver.InvokeActionResponse{}
+
+	action, diags := s.FrameworkServer.Action(ctx, proto5Req.ActionType)
+
+	fwResp.Diagnostics.Append(diags...)
+
+	if fwResp.Diagnostics.HasError() {
+		return invokeActionErrorDiagnostics(ctx, fwResp.Diagnostics)
+	}
+
+	actionSchema, diags := s.FrameworkServer.ActionSchema(ctx, proto5Req.ActionType)
+
+	fwResp.Diagnostics.Append(diags...)
+
+	if fwResp.Diagnostics.HasError() {
+		return invokeActionErrorDiagnostics(ctx, fwResp.Diagnostics)
+	}
+
+	fwReq, diags := fromproto5.InvokeActionRequest(ctx, proto5Req, action, actionSchema)
+
+	fwResp.Diagnostics.Append(diags...)
+
+	if fwResp.Diagnostics.HasError() {
+		return invokeActionErrorDiagnostics(ctx, fwResp.Diagnostics)
+	}
+
+	s.FrameworkServer.InvokeAction(ctx, fwReq, fwResp)
+
+	// TODO:Actions: This is a stub implementation, so we aren't currently exposing any streaming mechanism to the developer.
+	// That will eventually need to change to send progress events back to Terraform.
+	//
+	// This logic will likely need to be moved over to the "toproto" package as well.
+	protoStream := &tfprotov5.InvokeActionServerStream{
+		Events: func(push func(tfprotov5.InvokeActionEvent) bool) {
+			push(tfprotov5.InvokeActionEvent{
+				Type: tfprotov5.CompletedInvokeActionEventType{
+					Diagnostics: toproto5.Diagnostics(ctx, fwResp.Diagnostics),
+				},
+			})
+		},
+	}
+
+	return protoStream, nil
 }

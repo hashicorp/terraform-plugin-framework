6ed8df7d21952cfa675007fa03db5f770f331d9b
stephybun <steph@hashicorp.com>
2025-07-09 17:28:47 +0200

    list: add dynamic attribute type (#1180)
    * remove irrelevant comment on lists and maps of objects
    
    * add dynamic attribute to list schemas
    
    * update comment for dynamic attribute
    
    Co-authored-by: Austin Valle <austinvalle@gmail.com>
    
    ---------
    
    Co-authored-by: Austin Valle <austinvalle@gmail.com>

--- list/schema/dynamic_attribute.go
+++ list/schema/dynamic_attribute.go
@@ -4,14 +4,11 @@
 package schema
 
 import (
-	"context"
-
 	"github.com/hashicorp/terraform-plugin-go/tftypes"
 
 	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/internal/fwschema"
 	"github.com/hashicorp/terraform-plugin-framework/internal/fwschema/fwxschema"
-	"github.com/hashicorp/terraform-plugin-framework/internal/fwtype"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
@@ -19,40 +16,23 @@ import (
 
 // Ensure the implementation satisifies the desired interfaces.
 var (
-	_ Attribute                                    = ListAttribute{}
-	_ fwschema.AttributeWithValidateImplementation = ListAttribute{}
-	_ fwxschema.AttributeWithListValidators        = ListAttribute{}
+	_ Attribute                                = DynamicAttribute{}
+	_ fwxschema.AttributeWithDynamicValidators = DynamicAttribute{}
 )
 
-// ListAttribute represents a schema attribute that is a list with a single
-// element type. When retrieving the value for this attribute, use types.List
-// as the value type unless the CustomType field is set. The ElementType field
-// must be set.
+// DynamicAttribute represents a schema attribute that is a dynamic, rather
+// than a single static type. Static types are always preferable over dynamic
+// types in Terraform as practitioners will receive less helpful configuration
+// assistance from validation error diagnostics and editor integrations. When
+// retrieving the value for this attribute, use types.Dynamic as the value type
+// unless the CustomType field is set.
 //
-// Terraform configurations configure this attribute using expressions that
-// return a list or directly via square brace syntax.
-//
-//	# list of strings
-//	example_attribute = ["first", "second"]
-//
-// Terraform configurations reference this attribute using expressions that
-// accept a list or an element directly via square brace 0-based index syntax:
-//
-//	# first known element
-//	.example_attribute[0]
-type ListAttribute struct {
-	// ElementType is the type for all elements of the list. This field must be
-	// set.
-	//
-	// Element types that contain a dynamic type (i.e. types.Dynamic) are not supported.
-	// If underlying dynamic values are required, replace this attribute definition with
-	// DynamicAttribute instead.
-	ElementType attr.Type
-
+// The concrete value type for a dynamic is determined at runtime by Terraform, if defined in the configuration.
+type DynamicAttribute struct {
 	// CustomType enables the use of a custom attribute type in place of the
-	// default basetypes.ListType. When retrieving data, the basetypes.ListValuable
-	// associated with this custom type must be used in place of types.List.
-	CustomType basetypes.ListTypable
+	// default basetypes.DynamicType. When retrieving data, the basetypes.DynamicValuable
+	// associated with this custom type must be used in place of types.Dynamic.
+	CustomType basetypes.DynamicTypable
 
 	// Required indicates whether the practitioner must enter a value for
 	// this attribute or not. Required and Optional cannot both be true.
@@ -120,19 +100,19 @@ type ListAttribute struct {
 	// If the Type field points to a custom type that implements the
 	// xattr.TypeWithValidate interface, the validators defined in this field
 	// are run in addition to the validation defined by the type.
-	Validators []validator.List
+	Validators []validator.Dynamic
 }
 
-// ApplyTerraform5AttributePathStep returns the result of stepping into a list
-// index or an error.
-func (a ListAttribute) ApplyTerraform5AttributePathStep(step tftypes.AttributePathStep) (interface{}, error) {
+// ApplyTerraform5AttributePathStep always returns an error as it is not
+// possible to step further into a DynamicAttribute.
+func (a DynamicAttribute) ApplyTerraform5AttributePathStep(step tftypes.AttributePathStep) (interface{}, error) {
 	return a.GetType().ApplyTerraform5AttributePathStep(step)
 }
 
-// Equal returns true if the given Attribute is a ListAttribute
+// Equal returns true if the given Attribute is a DynamicAttribute
 // and all fields are equal.
-func (a ListAttribute) Equal(o fwschema.Attribute) bool {
-	if _, ok := o.(ListAttribute); !ok {
+func (a DynamicAttribute) Equal(o fwschema.Attribute) bool {
+	if _, ok := o.(DynamicAttribute); !ok {
 		return false
 	}
 
@@ -140,83 +120,68 @@ func (a ListAttribute) Equal(o fwschema.Attribute) bool {
 }
 
 // GetDeprecationMessage returns the DeprecationMessage field value.
-func (a ListAttribute) GetDeprecationMessage() string {
+func (a DynamicAttribute) GetDeprecationMessage() string {
 	return a.DeprecationMessage
 }
 
 // GetDescription returns the Description field value.
-func (a ListAttribute) GetDescription() string {
+func (a DynamicAttribute) GetDescription() string {
 	return a.Description
 }
 
 // GetMarkdownDescription returns the MarkdownDescription field value.
-func (a ListAttribute) GetMarkdownDescription() string {
+func (a DynamicAttribute) GetMarkdownDescription() string {
 	return a.MarkdownDescription
 }
 
-// GetType returns types.ListType or the CustomType field value if defined.
-func (a ListAttribute) GetType() attr.Type {
+// GetType returns types.DynamicType or the CustomType field value if defined.
+func (a DynamicAttribute) GetType() attr.Type {
 	if a.CustomType != nil {
 		return a.CustomType
 	}
 
-	return types.ListType{
-		ElemType: a.ElementType,
-	}
+	return types.DynamicType
 }
 
 // IsComputed returns false because it does not apply to ListResource schemas.
-func (a ListAttribute) IsComputed() bool {
+func (a DynamicAttribute) IsComputed() bool {
 	return false
 }
 
 // IsOptional returns the Optional field value.
-func (a ListAttribute) IsOptional() bool {
+func (a DynamicAttribute) IsOptional() bool {
 	return a.Optional
 }
 
 // IsRequired returns the Required field value.
-func (a ListAttribute) IsRequired() bool {
+func (a DynamicAttribute) IsRequired() bool {
 	return a.Required
 }
 
 // IsSensitive returns false because it does not apply to ListResource schemas.
-func (a ListAttribute) IsSensitive() bool {
+func (a DynamicAttribute) IsSensitive() bool {
 	return false
 }
 
-// IsWriteOnly returns false because it does not apply to ListResource schemas.
-func (a ListAttribute) IsWriteOnly() bool {
+// IsWriteOnly returns false as write-only attributes are not relevant to ephemeral resource schemas,
+// as these schemas describe data that is explicitly not saved to any artifact.
+func (a DynamicAttribute) IsWriteOnly() bool {
 	return false
+}
+
+// DynamicValidators returns the Validators field value.
+func (a DynamicAttribute) DynamicValidators() []validator.Dynamic {
+	return a.Validators
 }
 
 // IsRequiredForImport returns false as this behavior is only relevant
 // for managed resource identity schema attributes.
-func (a ListAttribute) IsRequiredForImport() bool {
+func (a DynamicAttribute) IsRequiredForImport() bool {
 	return false
 }
 
 // IsOptionalForImport returns false as this behavior is only relevant
 // for managed resource identity schema attributes.
-func (a ListAttribute) IsOptionalForImport() bool {
+func (a DynamicAttribute) IsOptionalForImport() bool {
 	return false
-}
-
-// ListValidators returns the Validators field value.
-func (a ListAttribute) ListValidators() []validator.List {
-	return a.Validators
-}
-
-// ValidateImplementation contains logic for validating the
-// provider-defined implementation of the attribute to prevent unexpected
-// errors or panics. This logic runs during the GetProviderSchema RPC
-// and should never include false positives.
-func (a ListAttribute) ValidateImplementation(ctx context.Context, req fwschema.ValidateImplementationRequest, resp *fwschema.ValidateImplementationResponse) {
-	if a.CustomType == nil && a.ElementType == nil {
-		resp.Diagnostics.Append(fwschema.AttributeMissingElementTypeDiag(req.Path))
-	}
-
-	if a.CustomType == nil && fwtype.ContainsCollectionWithDynamic(a.GetType()) {
-		resp.Diagnostics.Append(fwtype.AttributeCollectionWithDynamicTypeDiag(req.Path))
-	}
 }

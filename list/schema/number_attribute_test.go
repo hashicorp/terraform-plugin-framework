8c780f17776b348acf1a379fb1db52a52b156b7e
stephybun <steph@hashicorp.com>
2025-07-09 09:28:10 +0200

    list: add primitive, list and map types (#1177)
    * add primitive types as well as list and map collection types
    
    * sort the listresources result to prevent false positives in test
    
    * omit mention of Computed in the code comment for the Required attribute
    
    * add number type

---
+++ list/schema/number_attribute_test.go
@@ -0,0 +1,470 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package schema_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/internal/fwschema"
+	"github.com/hashicorp/terraform-plugin-framework/internal/testing/testschema"
+	"github.com/hashicorp/terraform-plugin-framework/internal/testing/testtypes"
+	"github.com/hashicorp/terraform-plugin-framework/list/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+func TestNumberAttributeApplyTerraform5AttributePathStep(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute     schema.NumberAttribute
+		step          tftypes.AttributePathStep
+		expected      any
+		expectedError error
+	}{
+		"AttributeName": {
+			attribute:     schema.NumberAttribute{},
+			step:          tftypes.AttributeName("test"),
+			expected:      nil,
+			expectedError: fmt.Errorf("cannot apply AttributePathStep tftypes.AttributeName to basetypes.NumberType"),
+		},
+		"ElementKeyInt": {
+			attribute:     schema.NumberAttribute{},
+			step:          tftypes.ElementKeyInt(1),
+			expected:      nil,
+			expectedError: fmt.Errorf("cannot apply AttributePathStep tftypes.ElementKeyInt to basetypes.NumberType"),
+		},
+		"ElementKeyString": {
+			attribute:     schema.NumberAttribute{},
+			step:          tftypes.ElementKeyString("test"),
+			expected:      nil,
+			expectedError: fmt.Errorf("cannot apply AttributePathStep tftypes.ElementKeyString to basetypes.NumberType"),
+		},
+		"ElementKeyValue": {
+			attribute:     schema.NumberAttribute{},
+			step:          tftypes.ElementKeyValue(tftypes.NewValue(tftypes.String, "test")),
+			expected:      nil,
+			expectedError: fmt.Errorf("cannot apply AttributePathStep tftypes.ElementKeyValue to basetypes.NumberType"),
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got, err := testCase.attribute.ApplyTerraform5AttributePathStep(testCase.step)
+
+			if err != nil {
+				if testCase.expectedError == nil {
+					t.Fatalf("expected no error, got: %s", err)
+				}
+
+				if !strings.Contains(err.Error(), testCase.expectedError.Error()) {
+					t.Fatalf("expected error %q, got: %s", testCase.expectedError, err)
+				}
+			}
+
+			if err == nil && testCase.expectedError != nil {
+				t.Fatalf("got no error, expected: %s", testCase.expectedError)
+			}
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestNumberAttributeGetDeprecationMessage(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.NumberAttribute
+		expected  string
+	}{
+		"no-deprecation-message": {
+			attribute: schema.NumberAttribute{},
+			expected:  "",
+		},
+		"deprecation-message": {
+			attribute: schema.NumberAttribute{
+				DeprecationMessage: "test deprecation message",
+			},
+			expected: "test deprecation message",
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.GetDeprecationMessage()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestNumberAttributeEqual(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.NumberAttribute
+		other     fwschema.Attribute
+		expected  bool
+	}{
+		"different-type": {
+			attribute: schema.NumberAttribute{},
+			other:     testschema.AttributeWithNumberValidators{},
+			expected:  false,
+		},
+		"equal": {
+			attribute: schema.NumberAttribute{},
+			other:     schema.NumberAttribute{},
+			expected:  true,
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.Equal(testCase.other)
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestNumberAttributeGetDescription(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.NumberAttribute
+		expected  string
+	}{
+		"no-description": {
+			attribute: schema.NumberAttribute{},
+			expected:  "",
+		},
+		"description": {
+			attribute: schema.NumberAttribute{
+				Description: "test description",
+			},
+			expected: "test description",
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.GetDescription()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestNumberAttributeGetMarkdownDescription(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.NumberAttribute
+		expected  string
+	}{
+		"no-markdown-description": {
+			attribute: schema.NumberAttribute{},
+			expected:  "",
+		},
+		"markdown-description": {
+			attribute: schema.NumberAttribute{
+				MarkdownDescription: "test description",
+			},
+			expected: "test description",
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.GetMarkdownDescription()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestNumberAttributeGetType(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.NumberAttribute
+		expected  attr.Type
+	}{
+		"base": {
+			attribute: schema.NumberAttribute{},
+			expected:  types.NumberType,
+		},
+		"custom-type": {
+			attribute: schema.NumberAttribute{
+				CustomType: testtypes.NumberType{},
+			},
+			expected: testtypes.NumberType{},
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.GetType()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestNumberAttributeIsComputed(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.NumberAttribute
+		expected  bool
+	}{
+		"not-computed": {
+			attribute: schema.NumberAttribute{},
+			expected:  false,
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.IsComputed()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestNumberAttributeIsOptional(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.NumberAttribute
+		expected  bool
+	}{
+		"not-optional": {
+			attribute: schema.NumberAttribute{},
+			expected:  false,
+		},
+		"optional": {
+			attribute: schema.NumberAttribute{
+				Optional: true,
+			},
+			expected: true,
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.IsOptional()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestNumberAttributeIsRequired(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.NumberAttribute
+		expected  bool
+	}{
+		"not-required": {
+			attribute: schema.NumberAttribute{},
+			expected:  false,
+		},
+		"required": {
+			attribute: schema.NumberAttribute{
+				Required: true,
+			},
+			expected: true,
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.IsRequired()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestNumberAttributeIsSensitive(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.NumberAttribute
+		expected  bool
+	}{
+		"not-sensitive": {
+			attribute: schema.NumberAttribute{},
+			expected:  false,
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.IsSensitive()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestNumberAttributeIsWriteOnly(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.NumberAttribute
+		expected  bool
+	}{
+		"not-writeOnly": {
+			attribute: schema.NumberAttribute{},
+			expected:  false,
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.IsWriteOnly()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestNumberAttributeNumberValidators(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.NumberAttribute
+		expected  []validator.Number
+	}{
+		"no-validators": {
+			attribute: schema.NumberAttribute{},
+			expected:  nil,
+		},
+		"validators": {
+			attribute: schema.NumberAttribute{
+				Validators: []validator.Number{},
+			},
+			expected: []validator.Number{},
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.NumberValidators()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestNumberAttributeIsRequiredForImport(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.NumberAttribute
+		expected  bool
+	}{
+		"not-requiredForImport": {
+			attribute: schema.NumberAttribute{},
+			expected:  false,
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.IsRequiredForImport()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestNumberAttributeIsOptionalForImport(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		attribute schema.NumberAttribute
+		expected  bool
+	}{
+		"not-optionalForImport": {
+			attribute: schema.NumberAttribute{},
+			expected:  false,
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.attribute.IsOptionalForImport()
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}

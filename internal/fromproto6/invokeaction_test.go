e43f5769dd73a6e88aeff437c4def56cf5e983b9
Austin Valle <austinvalle@gmail.com>
2025-07-14 08:10:58 -0400

    from invoke

--- internal/fromproto6/invokeaction_test.go
+++ internal/fromproto6/invokeaction_test.go
@@ -3,4 +3,118 @@
 
 package fromproto6_test
 
-// TODO:Actions: Add unit tests once this mapping logic is complete
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+
+	"github.com/hashicorp/terraform-plugin-framework/action"
+	"github.com/hashicorp/terraform-plugin-framework/action/schema"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/internal/fromproto6"
+	"github.com/hashicorp/terraform-plugin-framework/internal/fwschema"
+	"github.com/hashicorp/terraform-plugin-framework/internal/fwserver"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+)
+
+func TestInvokeActionRequest(t *testing.T) {
+	t.Parallel()
+
+	testProto6Type := tftypes.Object{
+		AttributeTypes: map[string]tftypes.Type{
+			"test_attribute": tftypes.String,
+		},
+	}
+
+	testProto6Value := tftypes.NewValue(testProto6Type, map[string]tftypes.Value{
+		"test_attribute": tftypes.NewValue(tftypes.String, "test-value"),
+	})
+
+	testProto6DynamicValue, err := tfprotov6.NewDynamicValue(testProto6Type, testProto6Value)
+
+	if err != nil {
+		t.Fatalf("unexpected error calling tfprotov6.NewDynamicValue(): %s", err)
+	}
+
+	testUnlinkedSchema := schema.UnlinkedSchema{
+		Attributes: map[string]schema.Attribute{
+			"test_attribute": schema.StringAttribute{
+				Required: true,
+			},
+		},
+	}
+
+	testCases := map[string]struct {
+		input               *tfprotov6.InvokeActionRequest
+		actionSchema        fwschema.Schema
+		actionImpl          action.Action
+		providerMetaSchema  fwschema.Schema
+		expected            *fwserver.InvokeActionRequest
+		expectedDiagnostics diag.Diagnostics
+	}{
+		"nil": {
+			input:    nil,
+			expected: nil,
+		},
+		"empty": {
+			input:    &tfprotov6.InvokeActionRequest{},
+			expected: nil,
+			expectedDiagnostics: diag.Diagnostics{
+				diag.NewErrorDiagnostic(
+					"Missing Action Schema",
+					"An unexpected error was encountered when handling the request. "+
+						"This is always an issue in terraform-plugin-framework used to implement the provider and should be reported to the provider developers.\n\n"+
+						"Please report this to the provider developer:\n\n"+
+						"Missing schema.",
+				),
+			},
+		},
+		"config-missing-schema": {
+			input: &tfprotov6.InvokeActionRequest{
+				Config: &testProto6DynamicValue,
+			},
+			expected: nil,
+			expectedDiagnostics: diag.Diagnostics{
+				diag.NewErrorDiagnostic(
+					"Missing Action Schema",
+					"An unexpected error was encountered when handling the request. "+
+						"This is always an issue in terraform-plugin-framework used to implement the provider and should be reported to the provider developers.\n\n"+
+						"Please report this to the provider developer:\n\n"+
+						"Missing schema.",
+				),
+			},
+		},
+		"config": {
+			input: &tfprotov6.InvokeActionRequest{
+				Config: &testProto6DynamicValue,
+			},
+			actionSchema: testUnlinkedSchema,
+			expected: &fwserver.InvokeActionRequest{
+				Config: &tfsdk.Config{
+					Raw:    testProto6Value,
+					Schema: testUnlinkedSchema,
+				},
+				ActionSchema: testUnlinkedSchema,
+			},
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got, diags := fromproto6.InvokeActionRequest(context.Background(), testCase.input, testCase.actionImpl, testCase.actionSchema)
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+
+			if diff := cmp.Diff(diags, testCase.expectedDiagnostics); diff != "" {
+				t.Errorf("unexpected diagnostics difference: %s", diff)
+			}
+		})
+	}
+}

2f341cb9c717596ce6051c6ac0a293960ff74e9d
Austin Valle <austinvalle@gmail.com>
2025-07-17 10:02:09 -0400

    external interface

--- action/action.go
+++ action/action.go
@@ -49,3 +49,33 @@ type ActionWithModifyPlan interface {
 	// diagnostics to practitioners, such as validation errors.
 	ModifyPlan(context.Context, ModifyPlanRequest, *ModifyPlanResponse)
 }
+
+// ActionWithConfigValidators is an interface type that extends Action to include declarative validations.
+//
+// Declaring validation using this methodology simplifies implementation of
+// reusable functionality. These also include descriptions, which can be used
+// for automating documentation.
+//
+// Validation will include ConfigValidators and ValidateConfig, if both are
+// implemented, in addition to any Attribute or Type validation.
+type ActionWithConfigValidators interface {
+	Action
+
+	// ConfigValidators returns a list of functions which will all be performed during validation.
+	ConfigValidators(context.Context) []ConfigValidator
+}
+
+// ActionWithValidateConfig is an interface type that extends Action to include imperative validation.
+//
+// Declaring validation using this methodology simplifies one-off
+// functionality that typically applies to a single action. Any documentation
+// of this functionality must be manually added into schema descriptions.
+//
+// Validation will include ConfigValidators and ValidateConfig, if both are
+// implemented, in addition to any Attribute or Type validation.
+type ActionWithValidateConfig interface {
+	Action
+
+	// ValidateConfig performs the validation.
+	ValidateConfig(context.Context, ValidateConfigRequest, *ValidateConfigResponse)
+}

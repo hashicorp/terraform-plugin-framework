e4bcbbfc7ffb948a526be00c68806fe2d1bdad6e
Austin Valle <austinvalle@gmail.com>
2025-07-17 11:03:05 -0400

    proto5 and proto6 impl

---
+++ internal/fromproto5/validateactionconfig_test.go
@@ -0,0 +1,108 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package fromproto5_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform-plugin-framework/action"
+	"github.com/hashicorp/terraform-plugin-framework/action/schema"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/internal/fromproto5"
+	"github.com/hashicorp/terraform-plugin-framework/internal/fwschema"
+	"github.com/hashicorp/terraform-plugin-framework/internal/fwserver"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+)
+
+func TestValidateActionConfigRequest(t *testing.T) {
+	t.Parallel()
+
+	testProto5Type := tftypes.Object{
+		AttributeTypes: map[string]tftypes.Type{
+			"test_attribute": tftypes.String,
+		},
+	}
+
+	testProto5Value := tftypes.NewValue(testProto5Type, map[string]tftypes.Value{
+		"test_attribute": tftypes.NewValue(tftypes.String, "test-value"),
+	})
+
+	testProto5DynamicValue, err := tfprotov5.NewDynamicValue(testProto5Type, testProto5Value)
+
+	if err != nil {
+		t.Fatalf("unexpected error calling tfprotov5.NewDynamicValue(): %s", err)
+	}
+
+	testFwSchema := schema.UnlinkedSchema{
+		Attributes: map[string]schema.Attribute{
+			"test_attribute": schema.StringAttribute{
+				Required: true,
+			},
+		},
+	}
+
+	testCases := map[string]struct {
+		input               *tfprotov5.ValidateActionConfigRequest
+		actionSchema        fwschema.Schema
+		actionImpl          action.Action
+		expected            *fwserver.ValidateActionConfigRequest
+		expectedDiagnostics diag.Diagnostics
+	}{
+		"nil": {
+			input:    nil,
+			expected: nil,
+		},
+		"empty": {
+			input:    &tfprotov5.ValidateActionConfigRequest{},
+			expected: &fwserver.ValidateActionConfigRequest{},
+		},
+		"config-missing-schema": {
+			input: &tfprotov5.ValidateActionConfigRequest{
+				Config: &testProto5DynamicValue,
+			},
+			expected: &fwserver.ValidateActionConfigRequest{},
+			expectedDiagnostics: diag.Diagnostics{
+				diag.NewErrorDiagnostic(
+					"Unable to Convert Configuration",
+					"An unexpected error was encountered when converting the configuration from the protocol type. "+
+						"This is always an issue in terraform-plugin-framework used to implement the provider and should be reported to the provider developers.\n\n"+
+						"Please report this to the provider developer:\n\n"+
+						"Missing schema.",
+				),
+			},
+		},
+		"config": {
+			input: &tfprotov5.ValidateActionConfigRequest{
+				Config: &testProto5DynamicValue,
+			},
+			actionSchema: testFwSchema,
+			expected: &fwserver.ValidateActionConfigRequest{
+				Config: &tfsdk.Config{
+					Raw:    testProto5Value,
+					Schema: testFwSchema,
+				},
+			},
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got, diags := fromproto5.ValidateActionConfigRequest(context.Background(), testCase.input, testCase.actionImpl, testCase.actionSchema)
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+
+			if diff := cmp.Diff(diags, testCase.expectedDiagnostics); diff != "" {
+				t.Errorf("unexpected diagnostics difference: %s", diff)
+			}
+		})
+	}
+}

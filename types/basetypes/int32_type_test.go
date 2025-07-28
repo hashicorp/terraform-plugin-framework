b3515eb47b02bc8728f0529821879d1e78936635
Austin Valle <austinvalle@gmail.com>
2025-07-28 17:16:33 -0400

    fix tests with extra nil check

--- types/basetypes/int32_type_test.go
+++ types/basetypes/int32_type_test.go
@@ -78,7 +78,7 @@ func TestInt32TypeValueFromTerraform(t *testing.T) {
 				// expectations, we're good
 				return
 			}
-			if err == nil && test.expectedErr != "" {
+			if test.expectedErr != "" {
 				t.Errorf("Expected error to be %q, didn't get an error", test.expectedErr)
 				return
 			}

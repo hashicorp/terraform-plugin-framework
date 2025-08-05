5ea07eee2966d51c5132430ae0c55342d985817b
Austin Valle <austinvalle@gmail.com>
2025-08-05 18:59:52 -0400

    fromproto

--- internal/fromproto6/invokeaction.go
+++ internal/fromproto6/invokeaction.go
@@ -5,6 +5,7 @@ package fromproto6
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 
@@ -12,10 +13,11 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/internal/fwschema"
 	"github.com/hashicorp/terraform-plugin-framework/internal/fwserver"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 )
 
 // InvokeActionRequest returns the *fwserver.InvokeActionRequest equivalent of a *tfprotov6.InvokeActionRequest.
-func InvokeActionRequest(ctx context.Context, proto6 *tfprotov6.InvokeActionRequest, reqAction action.Action, actionSchema fwschema.Schema) (*fwserver.InvokeActionRequest, diag.Diagnostics) {
+func InvokeActionRequest(ctx context.Context, proto6 *tfprotov6.InvokeActionRequest, reqAction action.Action, actionSchema fwschema.Schema, linkedResourceSchemas []fwschema.Schema, linkedResourceIdentitySchemas []fwschema.Schema) (*fwserver.InvokeActionRequest, diag.Diagnostics) {
 	if proto6 == nil {
 		return nil, nil
 	}
@@ -47,7 +49,84 @@ func InvokeActionRequest(ctx context.Context, proto6 *tfprotov6.InvokeActionRequ
 
 	fw.Config = config
 
-	// TODO:Actions: Here we need to retrieve linked resource data
+	if len(proto6.LinkedResources) != len(linkedResourceSchemas) {
+		diags.AddError(
+			"Mismatched Linked Resources in InvokeAction Request",
+			"An unexpected error was encountered when handling the request. "+
+				"This is always a problem with Terraform or terraform-plugin-framework. Please report this to the provider developer.\n\n"+
+				fmt.Sprintf(
+					"Received %d linked resource(s), but the provider was expecting %d linked resource(s).",
+					len(proto6.LinkedResources),
+					len(linkedResourceSchemas),
+				),
+		)
+
+		return nil, diags
+	}
+
+	// MAINTAINER NOTE: The number of identity schemas should always be in sync (if not supported, will have nil),
+	// so this error check is more for panic prevention.
+	if len(proto6.LinkedResources) != len(linkedResourceIdentitySchemas) {
+		diags.AddError(
+			"Mismatched Linked Resources in InvokeAction Request",
+			"An unexpected error was encountered when handling the request. "+
+				"This is always a problem with Terraform or terraform-plugin-framework. Please report this to the provider developer.\n\n"+
+				fmt.Sprintf(
+					"Received %d linked resource(s), but the provider was expecting %d linked resource(s).",
+					len(proto6.LinkedResources),
+					len(linkedResourceIdentitySchemas),
+				),
+		)
+
+		return nil, diags
+	}
+
+	for i, linkedResource := range proto6.LinkedResources {
+		schema := linkedResourceSchemas[i]
+		identitySchema := linkedResourceIdentitySchemas[i]
+
+		// Config
+		config, configDiags := Config(ctx, linkedResource.Config, schema)
+		diags.Append(configDiags...)
+
+		// Prior state
+		priorState, priorStateDiags := State(ctx, linkedResource.PriorState, schema)
+		diags.Append(priorStateDiags...)
+
+		// Planned state (plan)
+		plannedState, plannedStateDiags := Plan(ctx, linkedResource.PlannedState, schema)
+		diags.Append(plannedStateDiags...)
+
+		// Planned identity
+		var plannedIdentity *tfsdk.ResourceIdentity
+		if linkedResource.PlannedIdentity != nil {
+			if identitySchema == nil {
+				// MAINTAINER NOTE: Not all linked resources support identity, so it's valid for an identity schema to be nil. However,
+				// it's not valid for Terraform core to send an identity for a linked resource that doesn't support one. This would likely indicate
+				// that there is a bug in the definition of the linked resources (not including an identity schema when it is supported), or a bug in
+				// either Terraform core/Framework.
+				diags.AddError(
+					"Unable to Convert Linked Resource Identity",
+					"An unexpected error was encountered when converting a linked resource identity from the protocol type. "+
+						fmt.Sprintf("Linked resource (at index %d) contained identity data, but the resource doesn't support identity.\n\n", i)+
+						"This is always a problem with the provider and should be reported to the provider developer.",
+				)
+				return nil, diags
+			}
+
+			identityVal, plannedIdentityDiags := ResourceIdentity(ctx, linkedResource.PlannedIdentity, identitySchema)
+			diags.Append(plannedIdentityDiags...)
+
+			plannedIdentity = identityVal
+		}
+
+		fw.LinkedResources = append(fw.LinkedResources, &fwserver.InvokeActionRequestLinkedResource{
+			Config:          config,
+			PlannedState:    plannedState,
+			PriorState:      priorState,
+			PlannedIdentity: plannedIdentity,
+		})
+	}
 
 	return fw, diags
 }
--- internal/fromproto6/planaction_test.go
+++ internal/fromproto6/planaction_test.go
@@ -12,16 +12,26 @@ import (
 	"github.com/hashicorp/terraform-plugin-go/tftypes"
 
 	"github.com/hashicorp/terraform-plugin-framework/action"
-	"github.com/hashicorp/terraform-plugin-framework/action/schema"
+	actionschema "github.com/hashicorp/terraform-plugin-framework/action/schema"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/internal/fromproto6"
 	"github.com/hashicorp/terraform-plugin-framework/internal/fwschema"
 	"github.com/hashicorp/terraform-plugin-framework/internal/fwserver"
+	"github.com/hashicorp/terraform-plugin-framework/resource/identityschema"
+	resourceschema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 )
 
 func TestPlanActionRequest(t *testing.T) {
 	t.Parallel()
+
+	testEmptyProto6Value := tftypes.NewValue(tftypes.Object{AttributeTypes: map[string]tftypes.Type{}}, map[string]tftypes.Value{})
+
+	testEmptyProto6DynamicValue, err := tfprotov6.NewDynamicValue(tftypes.Object{}, testEmptyProto6Value)
+
+	if err != nil {
+		t.Fatalf("unexpected error calling tfprotov6.NewDynamicValue(): %s", err)
+	}
 
 	testProto6Type := tftypes.Object{
 		AttributeTypes: map[string]tftypes.Type{
@@ -39,21 +49,83 @@ func TestPlanActionRequest(t *testing.T) {
 		t.Fatalf("unexpected error calling tfprotov6.NewDynamicValue(): %s", err)
 	}
 
-	testUnlinkedSchema := schema.UnlinkedSchema{
-		Attributes: map[string]schema.Attribute{
-			"test_attribute": schema.StringAttribute{
+	testLinkedResourceProto6Type := tftypes.Object{
+		AttributeTypes: map[string]tftypes.Type{
+			"test_attribute_one": tftypes.String,
+			"test_attribute_two": tftypes.Bool,
+		},
+	}
+
+	testLinkedResourceProto6Value := tftypes.NewValue(testLinkedResourceProto6Type, map[string]tftypes.Value{
+		"test_attribute_one": tftypes.NewValue(tftypes.String, "test-value-1"),
+		"test_attribute_two": tftypes.NewValue(tftypes.Bool, true),
+	})
+
+	testLinkedResourceProto6DynamicValue, err := tfprotov6.NewDynamicValue(testLinkedResourceProto6Type, testLinkedResourceProto6Value)
+
+	if err != nil {
+		t.Fatalf("unexpected error calling tfprotov6.NewDynamicValue(): %s", err)
+	}
+
+	testLinkedResourceSchema := resourceschema.Schema{
+		Attributes: map[string]resourceschema.Attribute{
+			"test_attribute_one": resourceschema.StringAttribute{
+				Required: true,
+			},
+			"test_attribute_two": resourceschema.BoolAttribute{
 				Required: true,
 			},
 		},
 	}
 
+	testLinkedResourceIdentityProto6Type := tftypes.Object{
+		AttributeTypes: map[string]tftypes.Type{
+			"test_id": tftypes.String,
+		},
+	}
+
+	testLinkedResourceIdentityProto6Value := tftypes.NewValue(testLinkedResourceIdentityProto6Type, map[string]tftypes.Value{
+		"test_id": tftypes.NewValue(tftypes.String, "id-123"),
+	})
+
+	testLinkedResourceIdentityProto6DynamicValue, err := tfprotov6.NewDynamicValue(testLinkedResourceIdentityProto6Type, testLinkedResourceIdentityProto6Value)
+
+	if err != nil {
+		t.Fatalf("unexpected error calling tfprotov6.NewDynamicValue(): %s", err)
+	}
+
+	testLinkedResourceIdentitySchema := identityschema.Schema{
+		Attributes: map[string]identityschema.Attribute{
+			"test_id": identityschema.StringAttribute{
+				RequiredForImport: true,
+			},
+		},
+	}
+
+	testUnlinkedSchema := actionschema.UnlinkedSchema{
+		Attributes: map[string]actionschema.Attribute{
+			"test_attribute": actionschema.StringAttribute{
+				Required: true,
+			},
+		},
+	}
+
+	testLifecycleSchemaLinked := actionschema.LifecycleSchema{
+		Attributes: map[string]actionschema.Attribute{},
+		LinkedResource: actionschema.LinkedResource{
+			TypeName: "test_linked_resource",
+		},
+	}
+
 	testCases := map[string]struct {
-		input               *tfprotov6.PlanActionRequest
-		actionSchema        fwschema.Schema
-		actionImpl          action.Action
-		providerMetaSchema  fwschema.Schema
-		expected            *fwserver.PlanActionRequest
-		expectedDiagnostics diag.Diagnostics
+		input                         *tfprotov6.PlanActionRequest
+		actionSchema                  fwschema.Schema
+		actionImpl                    action.Action
+		linkedResourceSchemas         []fwschema.Schema
+		linkedResourceIdentitySchemas []fwschema.Schema
+		providerMetaSchema            fwschema.Schema
+		expected                      *fwserver.PlanActionRequest
+		expectedDiagnostics           diag.Diagnostics
 	}{
 		"nil": {
 			input:    nil,
@@ -124,13 +196,241 @@ func TestPlanActionRequest(t *testing.T) {
 				},
 			},
 		},
+		"linkedresource": {
+			input: &tfprotov6.PlanActionRequest{
+				Config: &testEmptyProto6DynamicValue,
+				LinkedResources: []*tfprotov6.ProposedLinkedResource{
+					{
+						PriorState:   &testLinkedResourceProto6DynamicValue,
+						PlannedState: &testLinkedResourceProto6DynamicValue,
+						Config:       &testLinkedResourceProto6DynamicValue,
+						PriorIdentity: &tfprotov6.ResourceIdentityData{
+							IdentityData: &testLinkedResourceIdentityProto6DynamicValue,
+						},
+					},
+				},
+			},
+			linkedResourceSchemas: []fwschema.Schema{
+				testLinkedResourceSchema,
+			},
+			linkedResourceIdentitySchemas: []fwschema.Schema{
+				testLinkedResourceIdentitySchema,
+			},
+			actionSchema: testLifecycleSchemaLinked,
+			expected: &fwserver.PlanActionRequest{
+				ActionSchema: testLifecycleSchemaLinked,
+				Config: &tfsdk.Config{
+					Raw:    testEmptyProto6Value,
+					Schema: testLifecycleSchemaLinked,
+				},
+				LinkedResources: []*fwserver.PlanActionRequestLinkedResource{
+					{
+						Config: &tfsdk.Config{
+							Raw:    testLinkedResourceProto6Value,
+							Schema: testLinkedResourceSchema,
+						},
+						PlannedState: &tfsdk.Plan{
+							Raw:    testLinkedResourceProto6Value,
+							Schema: testLinkedResourceSchema,
+						},
+						PriorState: &tfsdk.State{
+							Raw:    testLinkedResourceProto6Value,
+							Schema: testLinkedResourceSchema,
+						},
+						PriorIdentity: &tfsdk.ResourceIdentity{
+							Raw:    testLinkedResourceIdentityProto6Value,
+							Schema: testLinkedResourceIdentitySchema,
+						},
+					},
+				},
+			},
+		},
+		"linkedresources": {
+			input: &tfprotov6.PlanActionRequest{
+				Config: &testEmptyProto6DynamicValue,
+				LinkedResources: []*tfprotov6.ProposedLinkedResource{
+					{
+						PriorState:   &testLinkedResourceProto6DynamicValue,
+						PlannedState: &testLinkedResourceProto6DynamicValue,
+						Config:       &testLinkedResourceProto6DynamicValue,
+						PriorIdentity: &tfprotov6.ResourceIdentityData{
+							IdentityData: &testLinkedResourceIdentityProto6DynamicValue,
+						},
+					},
+					{
+						PriorState:   &testLinkedResourceProto6DynamicValue,
+						PlannedState: &testLinkedResourceProto6DynamicValue,
+						Config:       &testLinkedResourceProto6DynamicValue,
+					},
+				},
+			},
+			linkedResourceSchemas: []fwschema.Schema{
+				testLinkedResourceSchema,
+				testLinkedResourceSchema,
+			},
+			linkedResourceIdentitySchemas: []fwschema.Schema{
+				testLinkedResourceIdentitySchema,
+				nil, // Second resource doesn't have an identity
+			},
+			actionSchema: testLifecycleSchemaLinked,
+			expected: &fwserver.PlanActionRequest{
+				ActionSchema: testLifecycleSchemaLinked,
+				Config: &tfsdk.Config{
+					Raw:    testEmptyProto6Value,
+					Schema: testLifecycleSchemaLinked,
+				},
+				LinkedResources: []*fwserver.PlanActionRequestLinkedResource{
+					{
+						Config: &tfsdk.Config{
+							Raw:    testLinkedResourceProto6Value,
+							Schema: testLinkedResourceSchema,
+						},
+						PlannedState: &tfsdk.Plan{
+							Raw:    testLinkedResourceProto6Value,
+							Schema: testLinkedResourceSchema,
+						},
+						PriorState: &tfsdk.State{
+							Raw:    testLinkedResourceProto6Value,
+							Schema: testLinkedResourceSchema,
+						},
+						PriorIdentity: &tfsdk.ResourceIdentity{
+							Raw:    testLinkedResourceIdentityProto6Value,
+							Schema: testLinkedResourceIdentitySchema,
+						},
+					},
+					{
+						Config: &tfsdk.Config{
+							Raw:    testLinkedResourceProto6Value,
+							Schema: testLinkedResourceSchema,
+						},
+						PlannedState: &tfsdk.Plan{
+							Raw:    testLinkedResourceProto6Value,
+							Schema: testLinkedResourceSchema,
+						},
+						PriorState: &tfsdk.State{
+							Raw:    testLinkedResourceProto6Value,
+							Schema: testLinkedResourceSchema,
+						},
+					},
+				},
+			},
+		},
+		"linkedresources-mismatched-number-of-schemas": {
+			input: &tfprotov6.PlanActionRequest{
+				Config: &testEmptyProto6DynamicValue,
+				LinkedResources: []*tfprotov6.ProposedLinkedResource{
+					{
+						PriorState:   &testLinkedResourceProto6DynamicValue,
+						PlannedState: &testLinkedResourceProto6DynamicValue,
+						Config:       &testLinkedResourceProto6DynamicValue,
+						PriorIdentity: &tfprotov6.ResourceIdentityData{
+							IdentityData: &testLinkedResourceIdentityProto6DynamicValue,
+						},
+					},
+					{
+						PriorState:   &testLinkedResourceProto6DynamicValue,
+						PlannedState: &testLinkedResourceProto6DynamicValue,
+						Config:       &testLinkedResourceProto6DynamicValue,
+					},
+				},
+			},
+			linkedResourceSchemas: []fwschema.Schema{
+				testLinkedResourceSchema,
+			},
+			linkedResourceIdentitySchemas: []fwschema.Schema{
+				testLinkedResourceIdentitySchema,
+				nil, // Second resource doesn't have an identity
+			},
+			actionSchema: testLifecycleSchemaLinked,
+			expectedDiagnostics: diag.Diagnostics{
+				diag.NewErrorDiagnostic(
+					"Mismatched Linked Resources in PlanAction Request",
+					"An unexpected error was encountered when handling the request. "+
+						"This is always a problem with Terraform or terraform-plugin-framework. Please report this to the provider developer.\n\n"+
+						"Received 2 linked resource(s), but the provider was expecting 1 linked resource(s).",
+				),
+			},
+		},
+		"linkedresources-mismatched-number-of-identity-schemas": {
+			input: &tfprotov6.PlanActionRequest{
+				Config: &testEmptyProto6DynamicValue,
+				LinkedResources: []*tfprotov6.ProposedLinkedResource{
+					{
+						PriorState:   &testLinkedResourceProto6DynamicValue,
+						PlannedState: &testLinkedResourceProto6DynamicValue,
+						Config:       &testLinkedResourceProto6DynamicValue,
+						PriorIdentity: &tfprotov6.ResourceIdentityData{
+							IdentityData: &testLinkedResourceIdentityProto6DynamicValue,
+						},
+					},
+					{
+						PriorState:   &testLinkedResourceProto6DynamicValue,
+						PlannedState: &testLinkedResourceProto6DynamicValue,
+						Config:       &testLinkedResourceProto6DynamicValue,
+					},
+				},
+			},
+			linkedResourceSchemas: []fwschema.Schema{
+				testLinkedResourceSchema,
+				testLinkedResourceSchema,
+			},
+			linkedResourceIdentitySchemas: []fwschema.Schema{
+				testLinkedResourceIdentitySchema,
+			},
+			actionSchema: testLifecycleSchemaLinked,
+			expectedDiagnostics: diag.Diagnostics{
+				diag.NewErrorDiagnostic(
+					"Mismatched Linked Resources in PlanAction Request",
+					"An unexpected error was encountered when handling the request. "+
+						"This is always a problem with Terraform or terraform-plugin-framework. Please report this to the provider developer.\n\n"+
+						"Received 2 linked resource(s), but the provider was expecting 1 linked resource(s).",
+				),
+			},
+		},
+		"linkedresources-no-identity-schema": {
+			input: &tfprotov6.PlanActionRequest{
+				Config: &testEmptyProto6DynamicValue,
+				LinkedResources: []*tfprotov6.ProposedLinkedResource{
+					{
+						PriorState:   &testLinkedResourceProto6DynamicValue,
+						PlannedState: &testLinkedResourceProto6DynamicValue,
+						Config:       &testLinkedResourceProto6DynamicValue,
+						PriorIdentity: &tfprotov6.ResourceIdentityData{
+							IdentityData: &testLinkedResourceIdentityProto6DynamicValue,
+						},
+					},
+				},
+			},
+			linkedResourceSchemas: []fwschema.Schema{
+				testLinkedResourceSchema,
+			},
+			linkedResourceIdentitySchemas: []fwschema.Schema{
+				nil,
+			},
+			actionSchema: testLifecycleSchemaLinked,
+			expectedDiagnostics: diag.Diagnostics{
+				diag.NewErrorDiagnostic(
+					"Unable to Convert Linked Resource Identity",
+					"An unexpected error was encountered when converting a linked resource identity from the protocol type. "+
+						"Linked resource (at index 0) contained identity data, but the resource doesn't support identity.\n\n"+
+						"This is always a problem with the provider and should be reported to the provider developer.",
+				),
+			},
+		},
 	}
 
 	for name, testCase := range testCases {
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 
-			got, diags := fromproto6.PlanActionRequest(context.Background(), testCase.input, testCase.actionImpl, testCase.actionSchema)
+			got, diags := fromproto6.PlanActionRequest(
+				context.Background(),
+				testCase.input,
+				testCase.actionImpl,
+				testCase.actionSchema,
+				testCase.linkedResourceSchemas,
+				testCase.linkedResourceIdentitySchemas,
+			)
 
 			if diff := cmp.Diff(got, testCase.expected); diff != "" {
 				t.Errorf("unexpected difference: %s", diff)

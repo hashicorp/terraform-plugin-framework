e39d5775ca20c34ce87ebe08cf3de1aa6bb05f89
stephybun <steph@hashicorp.com>
2025-08-28 12:30:48 +0200

    list: framework changes to support list with sdkv2 resources (#1198)
    * framework changes support list in sdkv2
    
    * update Set and SetAtPath to accept values of tftypes.Value
    
    * move protov5 information into the resource metadataresponse
    
    * add tests for updated Set and SetAtPath functionality
    
    * remove redundant return statement
    
    * further clean up
    
    * add a schemas method for list
    
    * return expected and received types in diags and add tests for failure states
    
    * address review comments
    
    * updated expected error message
    
    * extend support for supplying raw schema types to proto6
    
    * review comments

--- internal/fwserver/server_listresources.go
+++ internal/fwserver/server_listresources.go
@@ -85,14 +85,28 @@ func (s *Server) ListResourceFuncs(ctx context.Context) (map[string]func() list.
 			continue
 		}
 
+		rawV5SchemasResp := list.RawV5SchemaResponse{}
+		if listResourceWithSchemas, ok := listResource.(list.ListResourceWithRawV5Schemas); ok {
+			listResourceWithSchemas.RawV5Schemas(ctx, list.RawV5SchemaRequest{}, &rawV5SchemasResp)
+		}
+
+		rawV6SchemasResp := list.RawV6SchemaResponse{}
+		if listResourceWithSchemas, ok := listResource.(list.ListResourceWithRawV6Schemas); ok {
+			listResourceWithSchemas.RawV6Schemas(ctx, list.RawV6SchemaRequest{}, &rawV6SchemasResp)
+		}
+
 		resourceFuncs, _ := s.ResourceFuncs(ctx)
 		if _, ok := resourceFuncs[typeName]; !ok {
-			s.listResourceFuncsDiags.AddError(
-				"ListResource Type Defined without a Matching Managed Resource Type",
-				fmt.Sprintf("The %s ListResource type name was returned, but no matching managed Resource type was defined. ", typeName)+
-					"This is always an issue with the provider and should be reported to the provider developers.",
-			)
-			continue
+			if (rawV5SchemasResp.ProtoV5Schema == nil || rawV5SchemasResp.ProtoV5IdentitySchema == nil) && (rawV6SchemasResp.ProtoV6Schema == nil || rawV6SchemasResp.ProtoV6IdentitySchema == nil) {
+				s.listResourceFuncsDiags.AddError(
+					"ListResource Type Defined without a Matching Managed Resource Type",
+					fmt.Sprintf("The %s ListResource type name was returned, but no matching managed Resource type was defined. ", typeName)+
+						"If the matching managed Resource type is not a framework resource either ProtoV5Schema and ProtoV5IdentitySchema must be specified in the RawV5Schemas method, "+
+						"or ProtoV6Schema and ProtoV6IdentitySchema must be specified in the RawV6Schemas method. "+
+						"This is always an issue with the provider and should be reported to the provider developers.",
+				)
+				continue
+			}
 		}
 
 		s.listResourceFuncs[typeName] = listResourceFunc

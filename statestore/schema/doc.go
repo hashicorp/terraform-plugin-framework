c704f944d68a57f4c1f0a3429c1b2136e978b9e1
Rain <rainskwan@gmail.com>
2026-01-29 11:49:55 -0500

    removing filed in other PR

--- statestore/schema/doc.go
+++
@@ -1,7 +0,0 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-
-// Package schema contains all available schema functionality for statestores.
-// StateStore schemas define the structure and value types for configuration data.
-// Schemas are implemented via the statestore.StateStore type Schema method.
-package schema

187c9b94e4b33c745f6da8b5f889e0b470cd97fb
Austin Valle <austinvalle@gmail.com>
2026-02-05 11:29:33 -0500

    toproto6

---
+++ internal/toproto6/deletestate_test.go
@@ -0,0 +1,67 @@
+// Copyright IBM Corp. 2021, 2025
+// SPDX-License-Identifier: MPL-2.0
+
+package toproto6_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/internal/fwserver"
+	"github.com/hashicorp/terraform-plugin-framework/internal/toproto6"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+)
+
+func TestDeleteStateResponse(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		input    *fwserver.DeleteStateResponse
+		expected *tfprotov6.DeleteStateResponse
+	}{
+		"nil": {
+			input:    nil,
+			expected: nil,
+		},
+		"empty": {
+			input:    &fwserver.DeleteStateResponse{},
+			expected: &tfprotov6.DeleteStateResponse{},
+		},
+		"diagnostics": {
+			input: &fwserver.DeleteStateResponse{
+				Diagnostics: diag.Diagnostics{
+					diag.NewWarningDiagnostic("test warning summary", "test warning details"),
+					diag.NewErrorDiagnostic("test error summary", "test error details"),
+				},
+			},
+			expected: &tfprotov6.DeleteStateResponse{
+				Diagnostics: []*tfprotov6.Diagnostic{
+					{
+						Severity: tfprotov6.DiagnosticSeverityWarning,
+						Summary:  "test warning summary",
+						Detail:   "test warning details",
+					},
+					{
+						Severity: tfprotov6.DiagnosticSeverityError,
+						Summary:  "test error summary",
+						Detail:   "test error details",
+					},
+				},
+			},
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := toproto6.DeleteStateResponse(context.Background(), testCase.input)
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}

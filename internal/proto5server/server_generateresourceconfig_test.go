2ee75aa70b8f3b6549bf4f8075c24cc8770dc9c7
Selena Goods <selena.goods@hashicorp.com>
2026-03-10 10:04:42 -0400

    Implement `GenerateResourceConfig` RPC and existing Terraform default logic  (#1281)
    * Implement `GenerateResourceConfig` RPC in protov5 and Core's default implementation
    
    * Update plugin-go dependency
    
    * Add implementation to `proto6server`
    
    * Enable `GenerateResourceConfig` server capability
    
    * Apply suggestions from code review
    
    Co-authored-by: Ansgar Mertens <ansgar@hashicorp.com>
    Co-authored-by: Austin Valle <austinvalle@gmail.com>
    
    * Remove unused code
    
    * Add error logging
    
    * Add maintainer notes about SDKv2 compatibility logic
    
    * Add error handling for `AttributeAtTerraformPath` and `BlockAtTerraformPath`
    
    * Add changelog
    
    * Refactor error condition
    
    * Update changelog with Terraform Core version
    
    * Update terraform-plugin-go dependency to v0.31.0
    
    * Update `go.sum`
    
    ---------
    
    Co-authored-by: Ansgar Mertens <ansgar@hashicorp.com>
    Co-authored-by: Austin Valle <austinvalle@gmail.com>

---
+++ internal/proto5server/server_generateresourceconfig_test.go
@@ -0,0 +1,127 @@
+// Copyright IBM Corp. 2021, 2026
+// SPDX-License-Identifier: MPL-2.0
+
+package proto5server
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+
+	"github.com/hashicorp/terraform-plugin-framework/internal/fwserver"
+	"github.com/hashicorp/terraform-plugin-framework/internal/testing/testprovider"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+)
+
+func TestServerGenerateResourceConfig(t *testing.T) {
+	t.Parallel()
+
+	testType := tftypes.Object{
+		AttributeTypes: map[string]tftypes.Type{
+			"test_computed": tftypes.String,
+			"test_required": tftypes.String,
+		},
+	}
+
+	testCurrentStateValue := testNewDynamicValue(t, testType, map[string]tftypes.Value{
+		"test_computed": tftypes.NewValue(tftypes.String, nil),
+		"test_required": tftypes.NewValue(tftypes.String, "test-currentstate-value"),
+	})
+
+	testEmptyDynamicValue := testNewDynamicValue(t, tftypes.Object{}, nil)
+
+	testSchema := schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"test_computed": schema.StringAttribute{
+				Computed: true,
+			},
+			"test_required": schema.StringAttribute{
+				Required: true,
+			},
+		},
+	}
+
+	testCases := map[string]struct {
+		server           *Server
+		request          *tfprotov5.GenerateResourceConfigRequest
+		expectedError    error
+		expectedResponse *tfprotov5.GenerateResourceConfigResponse
+	}{
+		"no-schema": {
+			server: &Server{
+				FrameworkServer: fwserver.Server{
+					Provider: &testprovider.Provider{
+						ResourcesMethod: func(_ context.Context) []func() resource.Resource {
+							return []func() resource.Resource{
+								func() resource.Resource {
+									return &testprovider.Resource{
+										SchemaMethod: func(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {},
+										MetadataMethod: func(_ context.Context, _ resource.MetadataRequest, resp *resource.MetadataResponse) {
+											resp.TypeName = "test_resource"
+										},
+									}
+								},
+							}
+						},
+					},
+				},
+			},
+			request: &tfprotov5.GenerateResourceConfigRequest{
+				State:    testEmptyDynamicValue,
+				TypeName: "test_resource",
+			},
+			expectedResponse: &tfprotov5.GenerateResourceConfigResponse{
+				Config: testEmptyDynamicValue,
+			},
+		},
+		"request-currentstate": {
+			server: &Server{
+				FrameworkServer: fwserver.Server{
+					Provider: &testprovider.Provider{
+						ResourcesMethod: func(_ context.Context) []func() resource.Resource {
+							return []func() resource.Resource{
+								func() resource.Resource {
+									return &testprovider.Resource{
+										SchemaMethod: func(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+											resp.Schema = testSchema
+										},
+										MetadataMethod: func(_ context.Context, _ resource.MetadataRequest, resp *resource.MetadataResponse) {
+											resp.TypeName = "test_resource"
+										},
+									}
+								},
+							}
+						},
+					},
+				},
+			},
+			request: &tfprotov5.GenerateResourceConfigRequest{
+				State:    testCurrentStateValue,
+				TypeName: "test_resource",
+			},
+			expectedResponse: &tfprotov5.GenerateResourceConfigResponse{
+				Config: testCurrentStateValue,
+			},
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got, err := testCase.server.GenerateResourceConfig(t.Context(), testCase.request)
+
+			if diff := cmp.Diff(testCase.expectedError, err); diff != "" {
+				t.Errorf("unexpected error difference: %s", diff)
+			}
+
+			if diff := cmp.Diff(testCase.expectedResponse, got); diff != "" {
+				t.Errorf("unexpected response difference: %s", diff)
+			}
+		})
+	}
+}
---
+++ internal/toproto5/generateresourceconfig_test.go
@@ -0,0 +1,106 @@
+// Copyright IBM Corp. 2021, 2026
+// SPDX-License-Identifier: MPL-2.0
+
+package toproto5_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/internal/fwserver"
+	"github.com/hashicorp/terraform-plugin-framework/internal/toproto5"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+)
+
+func TestGenerateResourceConfigResponse(t *testing.T) {
+	t.Parallel()
+
+	testProto5Type := tftypes.Object{
+		AttributeTypes: map[string]tftypes.Type{
+			"test_attribute": tftypes.String,
+		},
+	}
+
+	testProto5Value := tftypes.NewValue(testProto5Type, map[string]tftypes.Value{
+		"test_attribute": tftypes.NewValue(tftypes.String, "test-value"),
+	})
+
+	testProto5DynamicValue, err := tfprotov5.NewDynamicValue(testProto5Type, testProto5Value)
+
+	if err != nil {
+		t.Fatalf("unexpected error calling tfprotov5.NewDynamicValue(): %s", err)
+	}
+
+	testConfig := &tfsdk.Config{
+		Raw: testProto5Value,
+		Schema: schema.Schema{
+			Attributes: map[string]schema.Attribute{
+				"test_attribute": schema.StringAttribute{
+					Required: true,
+				},
+			},
+		},
+	}
+
+	testCases := map[string]struct {
+		input    *fwserver.GenerateResourceConfigResponse
+		expected *tfprotov5.GenerateResourceConfigResponse
+	}{
+		"nil": {
+			input:    nil,
+			expected: nil,
+		},
+		"empty": {
+			input:    &fwserver.GenerateResourceConfigResponse{},
+			expected: &tfprotov5.GenerateResourceConfigResponse{},
+		},
+		"diagnostics": {
+			input: &fwserver.GenerateResourceConfigResponse{
+				Diagnostics: diag.Diagnostics{
+					diag.NewWarningDiagnostic("test warning summary", "test warning details"),
+					diag.NewErrorDiagnostic("test error summary", "test error details"),
+				},
+			},
+			expected: &tfprotov5.GenerateResourceConfigResponse{
+				Diagnostics: []*tfprotov5.Diagnostic{
+					{
+						Severity: tfprotov5.DiagnosticSeverityWarning,
+						Summary:  "test warning summary",
+						Detail:   "test warning details",
+					},
+					{
+						Severity: tfprotov5.DiagnosticSeverityError,
+						Summary:  "test error summary",
+						Detail:   "test error details",
+					},
+				},
+			},
+		},
+		"generated config": {
+			input: &fwserver.GenerateResourceConfigResponse{
+				GeneratedConfig: testConfig,
+			},
+			expected: &tfprotov5.GenerateResourceConfigResponse{
+				Config: &testProto5DynamicValue,
+			},
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := toproto5.GenerateResourceConfigResponse(context.Background(), testCase.input)
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
